11b909baf70c5c18cd295173f0110e6775c0ad4a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-20 14:41:27 -0300

    feat: ensure AddAccount call HashGenerator with correct plaintext

--- src/domain/use-cases/account/add-account.ts
+++ src/domain/use-cases/account/add-account.ts
@@ -1,12 +1,14 @@
 import { CheckAccountByEmailRepository } from '@/domain/contracts/database/repositories/account'
+import { HashGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError } from '@/domain/error'
 
-type Setup = (accountRepository: CheckAccountByEmailRepository) => AddAccount
+type Setup = (accountRepository: CheckAccountByEmailRepository, hash: HashGenerator) => AddAccount
 type Input = { name: string, email: string, password: string }
 type Output = void
 export type AddAccount = (input: Input) => Promise<Output>
 
-export const addAccountUseCase: Setup = (accountRepository) => async ({ name, email, password }) => {
+export const addAccountUseCase: Setup = (accountRepository, hash) => async ({ name, email, password }) => {
   const emailExists = await accountRepository.checkByEmail({ email })
   if (emailExists) throw new FieldInUseError('email')
+  await hash.generate({ plaintext: password })
 }
