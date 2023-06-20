46c5f3e925b6f218c5127d5fbd097a7850471afc
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-20 14:35:30 -0300

    feat: ensure AddAccount throw FieldInUseError if CheckAccountByEmailRepository return true

--- src/domain/use-cases/account/add-account.ts
+++ src/domain/use-cases/account/add-account.ts
@@ -1,4 +1,5 @@
 import { CheckAccountByEmailRepository } from '@/domain/contracts/database/repositories/account'
+import { FieldInUseError } from '@/domain/error'
 
 type Setup = (accountRepository: CheckAccountByEmailRepository) => AddAccount
 type Input = { name: string, email: string, password: string }
@@ -6,5 +7,6 @@ type Output = void
 export type AddAccount = (input: Input) => Promise<Output>
 
 export const addAccountUseCase: Setup = (accountRepository) => async ({ name, email, password }) => {
-  await accountRepository.checkByEmail({ email })
+  const emailExists = await accountRepository.checkByEmail({ email })
+  if (emailExists) throw new FieldInUseError('email')
 }
