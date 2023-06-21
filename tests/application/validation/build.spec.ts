43a2704930adb008c2b1be9d87baa4c8f5bcda59
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-21 16:31:06 -0300

    test: ensure ValidationBuilder return a Required validation if required() is call

---
+++ tests/application/validation/build.spec.ts
@@ -0,0 +1,19 @@
+import { RequiredValidation, ValidationBuilder as Builder } from '@/application/validation'
+
+import faker from 'faker'
+
+describe('ValidationBuilder', () => {
+  let value: string
+  let fieldName: string
+
+  beforeAll(() => {
+    value = faker.database.column()
+    fieldName = faker.database.column()
+  })
+
+  it('should return a Required validation if required() is call', () => {
+    const validators = Builder.of(value, fieldName).required().build()
+
+    expect(validators).toEqual([new RequiredValidation(value, fieldName)])
+  })
+})
