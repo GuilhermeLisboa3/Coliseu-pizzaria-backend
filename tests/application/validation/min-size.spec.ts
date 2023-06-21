d1354fc1f663f38ccca90de663740ffff8be5ec4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-21 16:20:19 -0300

    test: ensure MinSizeValidation return undefined if length valid

--- tests/application/validation/min-size.spec.ts
+++ tests/application/validation/min-size.spec.ts
@@ -9,4 +9,20 @@ describe('MinSizeValidation', () => {
 
     expect(error).toEqual(new MinSizeError(5))
   })
+
+  it('should return undefined if length valid', () => {
+    const sut = new MinSizeValidation('any_v', 5)
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  it('should return undefined if length valid', () => {
+    const sut = new MinSizeValidation('any_value', 5)
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
 })
