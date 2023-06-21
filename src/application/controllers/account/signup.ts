6b244f0ab2ff7c7398fed12c40278703ee12c96c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-21 17:40:03 -0300

    feat: ensure SignUpController call addAccount with correct values

--- src/application/controllers/account/signup.ts
+++ src/application/controllers/account/signup.ts
@@ -1,8 +1,15 @@
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { AddAccount } from '@/domain/use-cases/account'
 
 type HttpRequest = { name: string, email: string, password: string }
 
 export class SignUpController {
+  constructor (private readonly addAccount: AddAccount) {}
+
+  async perform ({ email, name, password }: HttpRequest): Promise<void> {
+    await this.addAccount({ email, name, password })
+  }
+
   buildValidators ({ name, email, password }: HttpRequest): Validator[] {
     return [
       ...Builder.of(name, 'name').required().build(),
