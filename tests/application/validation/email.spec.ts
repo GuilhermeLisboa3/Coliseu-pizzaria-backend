a742dab614a7b1fe5892a09afd31983098d06aac
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-21 15:56:05 -0300

    test: ensure EmailValidation return InvalidFieldError if email is invalid

---
+++ tests/application/validation/email.spec.ts
@@ -0,0 +1,22 @@
+import { InvalidFieldError } from '@/application/errors'
+import { EmailValidation } from '@/application/validation'
+
+import faker from 'faker'
+
+describe('EmailValidation', () => {
+  let invalidEmail: string
+  let fieldName: string
+
+  beforeAll(() => {
+    invalidEmail = faker.random.word()
+    fieldName = faker.database.column()
+  })
+
+  it('should return InvalidFieldError if email is invalid', () => {
+    const sut = new EmailValidation(invalidEmail, fieldName)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new InvalidFieldError(fieldName))
+  })
+})
