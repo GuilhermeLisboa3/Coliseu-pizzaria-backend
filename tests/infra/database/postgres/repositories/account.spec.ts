fd3458d52acb90c74d5cf1ee8d5bfd3ab6eedb46
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-21 15:31:04 -0300

    test: ensure AccountRepository return true if email already exists

--- tests/infra/database/postgres/repositories/account.spec.ts
+++ tests/infra/database/postgres/repositories/account.spec.ts
@@ -1,9 +1,10 @@
 import { AccountRepository } from '@/infra/database/postgres/repositories'
 import { accountParams } from '@/tests/mocks'
+import { prisma } from '@/infra/database/postgres/helpers'
 
 describe('AccountRepository', () => {
   let sut: AccountRepository
-  const { email } = accountParams
+  const { email, name, password } = accountParams
 
   beforeEach(() => {
     sut = new AccountRepository()
@@ -14,6 +15,14 @@ describe('AccountRepository', () => {
       const result = await sut.checkByEmail({ email })
 
       expect(result).toBe(false)
+    })
+
+    it('should return true if email already exists', async () => {
+      await prisma.user.create({ data: { name, email, password } })
+
+      const result = await sut.checkByEmail({ email })
+
+      expect(result).toBe(true)
     })
   })
 })
