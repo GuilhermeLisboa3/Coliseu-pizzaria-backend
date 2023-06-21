3b13c11356f66f737e37eb96f69f91cde809cd64
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-21 17:47:17 -0300

    feat: ensure SignUpController return badRequest if addAccount return FieldInUseError

--- src/application/controllers/account/signup.ts
+++ src/application/controllers/account/signup.ts
@@ -1,4 +1,6 @@
+import { HttpResponse, badRequest } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { FieldInUseError } from '@/domain/error'
 import { AddAccount } from '@/domain/use-cases/account'
 
 type HttpRequest = { name: string, email: string, password: string }
@@ -6,8 +8,13 @@ type HttpRequest = { name: string, email: string, password: string }
 export class SignUpController {
   constructor (private readonly addAccount: AddAccount) {}
 
-  async perform ({ email, name, password }: HttpRequest): Promise<void> {
-    await this.addAccount({ email, name, password })
+  async perform ({ email, name, password }: HttpRequest): Promise<HttpResponse> {
+    try {
+      await this.addAccount({ email, name, password })
+      return { statusCode: 200, data: null }
+    } catch (error) {
+      return badRequest(new FieldInUseError('name'))
+    }
   }
 
   buildValidators ({ name, email, password }: HttpRequest): Validator[] {
