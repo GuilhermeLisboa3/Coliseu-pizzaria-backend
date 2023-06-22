c544f30f473faf1f0a9e641373577a27422bfd7a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 13:26:45 -0300

    test: ensure router /signup return 400 if email already exists

--- tests/main/routes/account.test.ts
+++ tests/main/routes/account.test.ts
@@ -1,7 +1,9 @@
 import { accountParams } from '@/tests/mocks'
 import { app } from '@/main/config/app'
+import { prisma } from '@/infra/database/postgres/helpers'
 
 import request from 'supertest'
+import { FieldInUseError } from '@/domain/error'
 
 describe('Account routes', () => {
   const { name, email, password } = accountParams
@@ -13,6 +15,16 @@ describe('Account routes', () => {
         .send({ name, email, password })
 
       expect(status).toBe(201)
+    })
+
+    it('should return 400 if email already exists', async () => {
+      await prisma.user.create({ data: { name, email, password } })
+      const { status, body: { error } } = await request(app)
+        .post('/signup')
+        .send({ name, email, password })
+
+      expect(status).toBe(400)
+      expect(error).toEqual(new FieldInUseError('email').message)
     })
   })
 })
