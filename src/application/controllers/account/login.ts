b18eb1098bb86306f119d6d09c4db13ba9eb4677
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 16:21:29 -0300

    feat: ensure LoginController build Validators correctly

--- src/application/controllers/account/login.ts
+++ src/application/controllers/account/login.ts
@@ -1,10 +1,18 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { Validator, ValidationBuilder as Builder } from '@/application/validation'
 
-type HttpRequest = { name: string, email: string, password: string }
+type HttpRequest = { email: string, password: string }
 
 export class LoginController extends Controller {
-  async perform ({ email, name, password }: HttpRequest): Promise<HttpResponse> {
+  async perform ({ email, password }: HttpRequest): Promise<HttpResponse> {
     return { statusCode: 200, data: null }
+  }
+
+  buildValidators ({ email, password }: HttpRequest): Validator[] {
+    return [
+      ...Builder.of(email, 'email').required().email().build(),
+      ...Builder.of(password, 'password').required().min(5).build()
+    ]
   }
 }
