1530d571c2cb42e7a4a5bb2bf296c7c2c666a39d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 11:45:59 -0300

    test: ensure ExpressRouterAdapter call handler if correct value

---
+++ tests/main/adapters/express-router.spec.ts
@@ -0,0 +1,37 @@
+import { expressRouterAdapter } from '@/main/adapters'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { Controller } from '@/application/controllers'
+
+import { mock } from 'jest-mock-extended'
+import faker from 'faker'
+
+describe('Express Router Adapter', () => {
+  let sut: RequestHandler
+  let req: Request
+  let res: Response
+  let next: NextFunction
+
+  let key: string
+  let value: string
+
+  const controller = mock<Controller>()
+
+  beforeAll(() => {
+    key = faker.random.word()
+    value = faker.random.words(6)
+  })
+
+  beforeEach(() => {
+    sut = expressRouterAdapter(controller)
+    req = getMockReq({ body: { [key]: value } })
+    res = getMockRes().res
+    next = getMockRes().next
+  })
+  it('should call handler if correct value', async () => {
+    await sut(req, res, next)
+
+    expect(controller.handle).toHaveBeenCalledWith({ [key]: value })
+    expect(controller.handle).toHaveBeenCalledTimes(1)
+  })
+})
