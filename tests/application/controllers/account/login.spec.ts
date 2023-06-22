444a995ad51e1c12eeb2f268da842ab35a88e73e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 16:22:38 -0300

    test: ensure LoginController build Validators correctly

--- tests/application/controllers/account/login.spec.ts
+++ tests/application/controllers/account/login.spec.ts
@@ -1,8 +1,11 @@
 import { LoginController } from '@/application/controllers/account'
 import { Controller } from '@/application/controllers'
+import { accountParams } from '@/tests/mocks'
+import { EmailValidation, MinSizeValidation, RequiredValidation } from '@/application/validation'
 
 describe('LoginController', () => {
   let sut: LoginController
+  const { email, password } = accountParams
 
   beforeEach(() => {
     sut = new LoginController()
@@ -10,5 +13,16 @@ describe('LoginController', () => {
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ email, password })
+
+    expect(validators).toEqual([
+      new RequiredValidation(email, 'email'),
+      new EmailValidation(email, 'email'),
+      new RequiredValidation(password, 'password'),
+      new MinSizeValidation(password, 5)
+    ])
   })
 })
