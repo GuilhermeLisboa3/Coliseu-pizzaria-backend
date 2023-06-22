90590817fea80f30c3347d915ea2f8f32ca6e984
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 14:32:15 -0300

    feat: ensure Authentication call HashComparer with correct values

--- src/domain/use-cases/account/authentication.ts
+++ src/domain/use-cases/account/authentication.ts
@@ -1,12 +1,14 @@
 import { LoadAccountByEmailRepository } from '@/domain/contracts/database/repositories/account'
+import { HashComparer } from '@/domain/contracts/gateways'
 import { AuthenticationError } from '@/domain/error'
 
-type Setup = (accountRepository: LoadAccountByEmailRepository) => Authentication
+type Setup = (accountRepository: LoadAccountByEmailRepository, hash: HashComparer) => Authentication
 type Input = { email: string, password: string }
 type Output = void
 export type Authentication = (input: Input) => Promise<Output>
 
-export const authenticationUseCase: Setup = (accountRepository) => async ({ email }) => {
+export const authenticationUseCase: Setup = (accountRepository, hash) => async ({ email, password }) => {
   const account = await accountRepository.loadByEmail({ email })
   if (!account) throw new AuthenticationError()
+  await hash.compare({ plaintext: password, digest: account.password })
 }
