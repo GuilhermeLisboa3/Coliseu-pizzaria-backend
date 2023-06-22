6aed79161cb06e44abc73ca2c180cb315e1c23d8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 17:58:52 -0300

    test: ensure route /login return 401 if account does not exists

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -5,6 +5,6 @@ type ExpressAdapter = (controller: Controller) => RequestHandler
 
 export const expressRouterAdapter: ExpressAdapter = controller => async (req, res) => {
   const { statusCode, data } = await controller.handle({ ...req.body })
-  const json = [201].includes(statusCode) ? data : { error: data.message }
+  const json = [201, 200].includes(statusCode) ? data : { error: data.message }
   return res.status(statusCode).json(json)
 }
--- tests/main/routes/account.test.ts
+++ tests/main/routes/account.test.ts
@@ -4,7 +4,7 @@ import { prisma } from '@/infra/database/postgres/helpers'
 
 import request from 'supertest'
 import { FieldInUseError } from '@/domain/error'
-import { RequiredFieldError } from '@/application/errors'
+import { RequiredFieldError, UnauthorizedError } from '@/application/errors'
 
 describe('Account routes', () => {
   const { name, email, password } = accountParams
@@ -46,6 +46,15 @@ describe('Account routes', () => {
         .send({ email, password })
 
       expect(status).toBe(200)
+    })
+
+    it('should return 401 if account does not exists', async () => {
+      const { status, body: { error } } = await request(app)
+        .post('/login')
+        .send({ email, password })
+
+      expect(status).toBe(401)
+      expect(error).toEqual(new UnauthorizedError().message)
     })
   })
 })
