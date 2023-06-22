83084d87ac1e0cd0e7e306ab342d6f43353f9027
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 11:21:11 -0300

    test: ensure Content Type Middleware return xml content type when forced

--- tests/main/middlewares/content-type.test.ts
+++ tests/main/middlewares/content-type.test.ts
@@ -8,4 +8,13 @@ describe('Content Type Middleware', () => {
 
     await request(app).get('/test_content_type').expect('content-type', /json/)
   })
+
+  it('should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app).get('/test_content_type_xml').expect('content-type', /xml/)
+  })
 })
