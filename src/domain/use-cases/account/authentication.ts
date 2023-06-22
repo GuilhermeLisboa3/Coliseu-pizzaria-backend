645f4f633ec5716bc527c26fc94fc22544f45c2c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 14:41:53 -0300

    feat: ensure Authentication call TokenGenerator with correct key

--- src/domain/use-cases/account/authentication.ts
+++ src/domain/use-cases/account/authentication.ts
@@ -1,15 +1,16 @@
 import { LoadAccountByEmailRepository } from '@/domain/contracts/database/repositories/account'
-import { HashComparer } from '@/domain/contracts/gateways'
+import { HashComparer, TokenGenerator } from '@/domain/contracts/gateways'
 import { AuthenticationError } from '@/domain/error'
 
-type Setup = (accountRepository: LoadAccountByEmailRepository, hash: HashComparer) => Authentication
+type Setup = (accountRepository: LoadAccountByEmailRepository, hash: HashComparer, token: TokenGenerator) => Authentication
 type Input = { email: string, password: string }
 type Output = void
 export type Authentication = (input: Input) => Promise<Output>
 
-export const authenticationUseCase: Setup = (accountRepository, hash) => async ({ email, password }) => {
+export const authenticationUseCase: Setup = (accountRepository, hash, token) => async ({ email, password }) => {
   const account = await accountRepository.loadByEmail({ email })
   if (!account) throw new AuthenticationError()
   const isValid = await hash.compare({ plaintext: password, digest: account.password })
   if (!isValid) throw new AuthenticationError()
+  await token.generate({ key: account.id })
 }
