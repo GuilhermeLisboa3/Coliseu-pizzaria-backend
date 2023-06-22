d97633a1d90c180898d3806ed9b14dcabc513286
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 11:10:31 -0300

    test: ensure CORS Middleware enable Cors

---
+++ tests/main/middlewares/cors.test.ts
@@ -0,0 +1,12 @@
+import { app } from '@/main/config'
+
+import request from 'supertest'
+
+describe('CORS Middleware', () => {
+  it('should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => { res.send() })
+
+    await request(app).get('/test_cors')
+      .expect('Access-control-Allow-origin', '*')
+  })
+})
