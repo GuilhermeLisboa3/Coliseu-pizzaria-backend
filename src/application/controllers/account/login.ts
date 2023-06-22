27d606be2a0b0686120ce20963da952e7f0863a0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 16:33:44 -0300

    feat: ensure LoginController call authentication with correct values

--- src/application/controllers/account/login.ts
+++ src/application/controllers/account/login.ts
@@ -1,11 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
 import { Validator, ValidationBuilder as Builder } from '@/application/validation'
+import { Authentication } from '@/domain/use-cases/account'
 
 type HttpRequest = { email: string, password: string }
 
 export class LoginController extends Controller {
+  constructor (private readonly authentication: Authentication) { super() }
+
   async perform ({ email, password }: HttpRequest): Promise<HttpResponse> {
+    await this.authentication({ email, password })
     return { statusCode: 200, data: null }
   }
 
