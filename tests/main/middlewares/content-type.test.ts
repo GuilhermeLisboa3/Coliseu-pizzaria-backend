21bc8d3112b9d8dabae0e7260cdc8ecbbcadd22b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 11:19:30 -0300

    test: ensure Content Type Middleware return default content type as json

---
+++ tests/main/middlewares/content-type.test.ts
@@ -0,0 +1,11 @@
+import { app } from '@/main/config'
+
+import request from 'supertest'
+
+describe('Content Type Middleware', () => {
+  it('should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => { res.send('') })
+
+    await request(app).get('/test_content_type').expect('content-type', /json/)
+  })
+})
