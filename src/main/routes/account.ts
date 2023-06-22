36eae17fc3d8bc2e752f485aa5e1648bba9a661c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-22 17:42:00 -0300

    feat: ensure route /login return 200 on success

--- src/main/routes/account.ts
+++ src/main/routes/account.ts
@@ -1,8 +1,9 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
-import { makeSignUpController } from '@/main/factories/application/controllers/account'
+import { makeSignUpController, makeLoginController } from '@/main/factories/application/controllers/account'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/signup', adapt(makeSignUpController()))
+  router.post('/login', adapt(makeLoginController()))
 }
