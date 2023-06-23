2b34181ba15a0c4f4fddb16d10c23e5663e5207c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-23 16:31:44 -0300

    feat: ensure addCategoryUseCase call AddCategoryRepository with correct name

---
+++ src/domain/contracts/database/repositories/category/add-category.ts
@@ -0,0 +1,9 @@
+export interface AddCategoryRepository {
+  create: (input: AddCategoryRepository.Input) => Promise<AddCategoryRepository.Output>
+}
+
+export namespace AddCategoryRepository {
+  export type Input = { name: string }
+
+  export type Output = { id: string, name: string }
+}
--- src/domain/use-cases/category/add-category.ts
+++ src/domain/use-cases/category/add-category.ts
@@ -1,7 +1,7 @@
-import { CheckCategoryByNameRepository } from '@/domain/contracts/database/repositories/category'
+import { CheckCategoryByNameRepository, AddCategoryRepository } from '@/domain/contracts/database/repositories/category'
 import { FieldInUseError } from '@/domain/error'
 
-type Setup = (CategoryRepository: CheckCategoryByNameRepository) => AddCategory
+type Setup = (CategoryRepository: CheckCategoryByNameRepository & AddCategoryRepository) => AddCategory
 type Input = { name: string }
 type Output = void
 export type AddCategory = (input: Input) => Promise<Output>
@@ -9,4 +9,5 @@ export type AddCategory = (input: Input) => Promise<Output>
 export const addCategoryUseCase: Setup = (categoryRepository) => async ({ name }) => {
   const category = await categoryRepository.checkByName({ name })
   if (category) throw new FieldInUseError('name')
+  await categoryRepository.create({ name })
 }
