7a5bfe1ca9b9dab0ed57564310cc92b104b615d6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-23 12:29:27 -0300

    feat: ensure AuthorizeUseCase return accountId on success

--- src/domain/use-cases/account/authorize.ts
+++ src/domain/use-cases/account/authorize.ts
@@ -4,7 +4,7 @@ import { AuthenticationError, PermissionError } from '@/domain/error'
 
 type Setup = (token: TokenValidator, accountRepository: CheckAccountByRole) => Authorize
 type Input = { accessToken: string, role?: string }
-type Output = void
+type Output = { accountId: string }
 export type Authorize = (input: Input) => Promise<Output>
 
 export const AuthorizeUseCase: Setup = (token, accountRepository) => async ({ accessToken, role }) => {
@@ -14,4 +14,5 @@ export const AuthorizeUseCase: Setup = (token, accountRepository) => async ({ ac
   } catch (error) { throw new AuthenticationError() }
   const account = await accountRepository.checkByRole({ accountId, role })
   if (!account) throw new PermissionError()
+  return { accountId }
 }
