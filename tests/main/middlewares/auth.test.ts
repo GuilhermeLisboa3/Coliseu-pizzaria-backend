06264fd010fa55bd75816a379707b5b9dc26a13f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-23 14:56:22 -0300

    test: ensure AuthMiddleware return 200 if authorization header is valid

--- tests/main/middlewares/auth.test.ts
+++ tests/main/middlewares/auth.test.ts
@@ -1,10 +1,14 @@
-import { app } from '@/main/config'
+import { app, env } from '@/main/config'
 import { auth } from '@/main/middlewares'
+import { accountParams } from '@/tests/mocks'
 import { UnauthorizedError } from '@/application/errors'
+import { prisma } from '@/infra/database/postgres/helpers'
 
 import request from 'supertest'
+import { sign } from 'jsonwebtoken'
 
 describe('Auth Middleware', () => {
+  const { id, name, email, password } = accountParams
   it('should return 401 if authorization header was not provided', async () => {
     app.get('/fake_route', auth)
 
@@ -12,5 +16,18 @@ describe('Auth Middleware', () => {
 
     expect(status).toBe(401)
     expect(body.error).toBe(new UnauthorizedError().message)
+  })
+
+  it('should return 200 if authorization header is valid', async () => {
+    const account = await prisma.user.create({ data: { id, name, email, password } })
+    const token = sign({ key: account.id }, env.secret)
+    app.get('/fake_route', auth, (req, res) => { res.json(req.locals) })
+
+    const { status, body } = await request(app)
+      .get('/fake_route')
+      .set({ authorization: `Bearer: ${token}` })
+
+    expect(status).toBe(200)
+    expect(body).toEqual({ accountId: id })
   })
 })
