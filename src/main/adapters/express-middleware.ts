f7d5345c5d4700e14803860b464939eef337df5a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-23 14:03:07 -0300

    feat: ensure expressMiddlewareAdapter add valid data to req.locals on success

--- src/main/adapters/express-middleware.ts
+++ src/main/adapters/express-middleware.ts
@@ -6,6 +6,8 @@ type ExpressAdapter = (middleware: Middleware) => RequestHandler
 export const expressMiddlewareAdapter: ExpressAdapter = middleware => async (req, res, next) => {
   const { statusCode, data } = await middleware.handle({ ...req.headers })
   if (statusCode === 200) {
+    const validEntries = Object.entries(data).filter(([, value]) => value)
+    req.locals = { ...req.locals, ...Object.fromEntries(validEntries) }
     next()
   } else {
     res.status(statusCode).json({ error: data.message })
