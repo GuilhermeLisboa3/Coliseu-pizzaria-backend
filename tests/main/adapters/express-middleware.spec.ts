6527f5daf694a436615081c8103545576d541315
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-23 13:52:58 -0300

    test: ensure expressMiddlewareAdapter call handle with empty value

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -34,4 +34,13 @@ describe('Express Middleware Adapter', () => {
     expect(middleware.handle).toHaveBeenCalledWith({ [key]: value })
     expect(middleware.handle).toHaveBeenCalledTimes(1)
   })
+
+  it('should call handle with empty value', async () => {
+    req = getMockReq()
+
+    await sut(req, res, next)
+
+    expect(middleware.handle).toHaveBeenCalledWith({})
+    expect(middleware.handle).toHaveBeenCalledTimes(1)
+  })
 })
