da6b01be7a29aa28abb7a74bb6a01c7697db0d8f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-23 11:42:47 -0300

    feat: ensure AuthorizeUseCase throw AuthenticationError if TokenValidator throws

--- src/domain/use-cases/account/authorize.ts
+++ src/domain/use-cases/account/authorize.ts
@@ -1,4 +1,5 @@
 import { TokenValidator } from '@/domain/contracts/gateways'
+import { AuthenticationError } from '@/domain/error'
 
 type Setup = (token: TokenValidator) => Authorize
 type Input = { accessToken: string }
@@ -6,5 +7,7 @@ type Output = void
 export type Authorize = (input: Input) => Promise<Output>
 
 export const AuthorizeUseCase: Setup = (token) => async ({ accessToken }) => {
-  await token.validate({ token: accessToken })
+  try {
+    await token.validate({ token: accessToken })
+  } catch (error) { throw new AuthenticationError() }
 }
