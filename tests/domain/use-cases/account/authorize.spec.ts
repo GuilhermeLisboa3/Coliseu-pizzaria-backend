26ae16e5c2db0af9c703fc80063d0b4afd394047
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-23 11:52:33 -0300

    test: ensure AuthorizeUseCase call CheckAccountByRole with correct values

--- tests/domain/use-cases/account/authorize.spec.ts
+++ tests/domain/use-cases/account/authorize.spec.ts
@@ -1,6 +1,7 @@
 import { accountParams } from '@/tests/mocks'
 import { Authorize, AuthorizeUseCase } from '@/domain/use-cases/account'
 import { TokenValidator } from '@/domain/contracts/gateways'
+import { CheckAccountByRole } from '@/domain/contracts/database/repositories/account'
 import { AuthenticationError } from '@/domain/error'
 
 import { mock } from 'jest-mock-extended'
@@ -8,15 +9,21 @@ import { mock } from 'jest-mock-extended'
 describe('AuthorizeUseCase', () => {
   let sut: Authorize
 
+  const role = 'admin'
   const token = mock<TokenValidator>()
-  const { accessToken, error } = accountParams
+  const accountRepository = mock<CheckAccountByRole>()
+  const { accessToken, error, id } = accountParams
+
+  beforeAll(() => {
+    token.validate.mockResolvedValue(id)
+  })
 
   beforeEach(() => {
-    sut = AuthorizeUseCase(token)
+    sut = AuthorizeUseCase(token, accountRepository)
   })
 
   it('should call TokenValidator with correct value', async () => {
-    await sut({ accessToken })
+    await sut({ accessToken, role })
 
     expect(token.validate).toHaveBeenCalledWith({ token: accessToken })
     expect(token.validate).toHaveBeenCalledTimes(1)
@@ -24,8 +31,15 @@ describe('AuthorizeUseCase', () => {
 
   it('should throw AuthenticationError if TokenValidator throws', async () => {
     token.validate.mockRejectedValueOnce(error)
-    const promise = sut({ accessToken })
+    const promise = sut({ accessToken, role })
 
     await expect(promise).rejects.toThrow(new AuthenticationError())
+  })
+
+  it('should call CheckAccountByRole with correct values', async () => {
+    await sut({ accessToken, role })
+
+    expect(accountRepository.checkByRole).toHaveBeenCalledWith({ accountId: id, role })
+    expect(accountRepository.checkByRole).toHaveBeenCalledTimes(1)
   })
 })
