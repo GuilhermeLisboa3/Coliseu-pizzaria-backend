cd21931078b1d7a95d4667e0414949f8d720d705
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 16:13:59 -0300

    feat: ensure AddProductUseCase call UploadFile with correct values

--- src/domain/use-cases/product/add-product.ts
+++ src/domain/use-cases/product/add-product.ts
@@ -1,21 +1,23 @@
 import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
-import { UUIDGenerator } from '@/domain/contracts/gateways'
+import { UUIDGenerator, UploadFile } from '@/domain/contracts/gateways'
 import { FieldInUseError, FieldNotFoundError } from '@/domain/error'
 
 type Setup = (
   productRepository: CheckProductByNameRepository,
   categoryRepository: CheckCategoryByIdRepository,
-  uuid: UUIDGenerator
+  uuid: UUIDGenerator,
+  fileStorage: UploadFile
 ) => AddProduct
-type Input = { name: string, categoryId: string }
+type Input = { categoryId: string, name: string, description: string, price: number, file?: { buffer: Buffer, mimeType: string } }
 type Output = void
 export type AddProduct = (input: Input) => Promise<Output>
 
-export const addProductUseCase: Setup = (productRepository, categoryRepository, uuid) => async ({ name, categoryId }) => {
+export const addProductUseCase: Setup = (productRepository, categoryRepository, uuid, fileStorage) => async ({ name, categoryId, description, price, file }) => {
   const productExists = await productRepository.checkByName({ name })
   if (productExists) throw new FieldInUseError('name')
   const categoryExists = await categoryRepository.checkById({ id: categoryId })
   if (!categoryExists) throw new FieldNotFoundError('categoryId')
-  uuid.generate()
+  const key = uuid.generate()
+  if (file) await fileStorage.upload({ file: file.buffer, fileName: `${key}.${file.mimeType.split('/')[1]}` })
 }
