a02f5d6e1a3055fb3da65476d8d590183426fbd3
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 14:08:27 -0300

    feat: ensure route /category/:id return 400 if id not exist

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -4,7 +4,7 @@ import { RequestHandler } from 'express'
 type ExpressAdapter = (controller: Controller) => RequestHandler
 
 export const expressRouterAdapter: ExpressAdapter = controller => async (req, res) => {
-  const { statusCode, data } = await controller.handle({ ...req.body, ...req.locals })
+  const { statusCode, data } = await controller.handle({ ...req.body, ...req.locals, ...req.params })
   const json = [201, 200, 204].includes(statusCode) ? data : { error: data.message }
   return res.status(statusCode).json(json)
 }
--- src/main/routes/category.ts
+++ src/main/routes/category.ts
@@ -1,9 +1,10 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
 import { authAdmin } from '@/main/middlewares'
-import { makeAddCategoryController } from '@/main/factories/application/controllers/category'
+import { makeAddCategoryController, makeDeleteCategoryControllerr } from '@/main/factories/application/controllers/category'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/category', authAdmin, adapt(makeAddCategoryController()))
+  router.delete('/category/:id', authAdmin, adapt(makeDeleteCategoryControllerr()))
 }
