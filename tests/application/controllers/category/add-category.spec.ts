cd4930225310c6dffb1c43b075fa2d9188bb62a1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 11:10:07 -0300

    test: ensure AddCategoryController return badRequest if addCategory return FieldInUseError

--- tests/application/controllers/category/add-category.spec.ts
+++ tests/application/controllers/category/add-category.spec.ts
@@ -2,14 +2,15 @@ import { categoryParams } from '@/tests/mocks'
 import { AddCategoryController } from '@/application/controllers/category'
 import { Controller } from '@/application/controllers'
 import { RequiredValidation } from '@/application/validation'
+import { FieldInUseError } from '@/domain/error'
 
 describe('AddCategoryController', () => {
   const { name } = categoryParams
-  const addAccount = jest.fn()
+  const addCategory = jest.fn()
   let sut: AddCategoryController
 
   beforeEach(() => {
-    sut = new AddCategoryController(addAccount)
+    sut = new AddCategoryController(addCategory)
   })
 
   it('should extend Controller', async () => {
@@ -24,10 +25,18 @@ describe('AddCategoryController', () => {
     ])
   })
 
-  it('should call addAccount with correct values', async () => {
+  it('should call addCategory with correct values', async () => {
     await sut.handle({ name })
 
-    expect(addAccount).toHaveBeenCalledWith({ name })
-    expect(addAccount).toHaveBeenCalledTimes(1)
+    expect(addCategory).toHaveBeenCalledWith({ name })
+    expect(addCategory).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return badRequest if addCategory return FieldInUseError', async () => {
+    addCategory.mockRejectedValueOnce(new FieldInUseError('name'))
+    const { statusCode, data } = await sut.handle({ name })
+
+    expect(statusCode).toBe(400)
+    expect(data).toEqual(new FieldInUseError('name'))
   })
 })
