a32eac4d95ebc2a928e5664b2b2f98e61a0510a6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 11:04:40 -0300

    feat: ensure AddCategoryController build Validators correctly

--- src/application/controllers/category/add-category.ts
+++ src/application/controllers/category/add-category.ts
@@ -1,10 +1,17 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { Validator, ValidationBuilder as Builder } from '@/application/validation'
 
 type HttpRequest = { name: string }
 
 export class AddCategoryController extends Controller {
   async perform ({ name }: HttpRequest): Promise<HttpResponse> {
     return { data: null, statusCode: 200 }
+  }
+
+  buildValidators ({ name }: HttpRequest): Validator[] {
+    return [
+      ...Builder.of(name, 'name').required().build()
+    ]
   }
 }
