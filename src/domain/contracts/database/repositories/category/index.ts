be75beb45637cb15d4f102b8fa9ba83f5cf4460a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 13:04:53 -0300

    feat: ensure DeleteCategoryUseCase call DeleteCategoryRepository with correct value

--- src/domain/contracts/database/repositories/category/index.ts
+++ src/domain/contracts/database/repositories/category/index.ts
@@ -1,3 +1,4 @@
 export * from './check-category-by-name'
 export * from './add-category'
 export * from './check-category-by-id'
+export * from './delete-category'
--- src/domain/use-cases/category/delete-category.ts
+++ src/domain/use-cases/category/delete-category.ts
@@ -1,7 +1,7 @@
-import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
+import { CheckCategoryByIdRepository, DeleteCategoryRepository } from '@/domain/contracts/database/repositories/category'
 import { FieldNotFoundError } from '@/domain/error'
 
-type Setup = (CategoryRepository: CheckCategoryByIdRepository) => DeleteCategory
+type Setup = (CategoryRepository: CheckCategoryByIdRepository & DeleteCategoryRepository) => DeleteCategory
 type Input = { id: string }
 type Output = void
 export type DeleteCategory = (input: Input) => Promise<Output>
@@ -9,4 +9,5 @@ export type DeleteCategory = (input: Input) => Promise<Output>
 export const deleteCategoryUseCase: Setup = (categoryRepository) => async ({ id }) => {
   const category = await categoryRepository.checkById({ id })
   if (!category) throw new FieldNotFoundError('id')
+  await categoryRepository.delete({ id })
 }
