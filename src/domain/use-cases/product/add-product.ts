802e68d633fc8d786aa99099c6498f0e5d2025ce
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 15:40:23 -0300

    feat: ensure AddProductUseCase call CheckCategoryByIdRepository with correct value

--- src/domain/use-cases/product/add-product.ts
+++ src/domain/use-cases/product/add-product.ts
@@ -1,12 +1,17 @@
+import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
 import { FieldInUseError } from '@/domain/error'
 
-type Setup = (productRepository: CheckProductByNameRepository) => AddProduct
-type Input = { name: string }
+type Setup = (
+  productRepository: CheckProductByNameRepository,
+  categoryRepository: CheckCategoryByIdRepository
+) => AddProduct
+type Input = { name: string, categoryId: string }
 type Output = void
 export type AddProduct = (input: Input) => Promise<Output>
 
-export const addProductUseCase: Setup = (productRepository) => async ({ name }) => {
+export const addProductUseCase: Setup = (productRepository, categoryRepository) => async ({ name, categoryId }) => {
   const productExists = await productRepository.checkByName({ name })
   if (productExists) throw new FieldInUseError('name')
+  await categoryRepository.checkById({ id: categoryId })
 }
