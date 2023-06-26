1a23c3fb59be043262a1dd404a2c31ad2f935b4c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 13:12:33 -0300

    feat: ensure CategoryRepository checkById return true if id already exists

--- src/infra/database/postgres/repositories/category.ts
+++ src/infra/database/postgres/repositories/category.ts
@@ -1,7 +1,7 @@
-import { CheckCategoryByNameRepository, AddCategoryRepository } from '@/domain/contracts/database/repositories/category'
+import { CheckCategoryByNameRepository, AddCategoryRepository, CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { prisma } from '@/infra/database/postgres/helpers'
 
-export class CategoryRepository implements CheckCategoryByNameRepository, AddCategoryRepository {
+export class CategoryRepository implements CheckCategoryByNameRepository, AddCategoryRepository, CheckCategoryByIdRepository {
   async checkByName ({ name }: CheckCategoryByNameRepository.Input): Promise<CheckCategoryByNameRepository.Output> {
     const isValid = await prisma.category.findFirst({ where: { name } })
 
@@ -12,5 +12,11 @@ export class CategoryRepository implements CheckCategoryByNameRepository, AddCat
     const category = await prisma.category.create({ data: { name } })
 
     return category
+  }
+
+  async checkById ({ id }: CheckCategoryByIdRepository.Input): Promise<CheckCategoryByIdRepository.Output> {
+    const isValid = await prisma.category.findFirst({ where: { id } })
+
+    return isValid != null
   }
 }
