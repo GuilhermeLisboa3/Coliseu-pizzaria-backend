9787553259ee1f87742c34bcc0afe79ed7a32b78
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 11:07:37 -0300

    feat: ensure AddCategoryController call addAccount with correct values

--- src/application/controllers/category/add-category.ts
+++ src/application/controllers/category/add-category.ts
@@ -1,11 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
 import { Validator, ValidationBuilder as Builder } from '@/application/validation'
+import { AddCategory } from '@/domain/use-cases/category'
 
 type HttpRequest = { name: string }
 
 export class AddCategoryController extends Controller {
+  constructor (private readonly addCategory: AddCategory) { super() }
+
   async perform ({ name }: HttpRequest): Promise<HttpResponse> {
+    await this.addCategory({ name })
     return { data: null, statusCode: 200 }
   }
 
