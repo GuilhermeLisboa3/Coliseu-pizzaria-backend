1db4eec856832ba5983967f14b22ecef5253d3eb
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 11:05:41 -0300

    test: ensure AddCategoryController build Validators correctly

--- tests/application/controllers/category/add-category.spec.ts
+++ tests/application/controllers/category/add-category.spec.ts
@@ -1,8 +1,11 @@
+import { categoryParams } from '@/tests/mocks'
 import { AddCategoryController } from '@/application/controllers/category'
 import { Controller } from '@/application/controllers'
+import { RequiredValidation } from '@/application/validation'
 
 describe('AddCategoryController', () => {
   let sut: AddCategoryController
+  const { name } = categoryParams
 
   beforeEach(() => {
     sut = new AddCategoryController()
@@ -10,5 +13,13 @@ describe('AddCategoryController', () => {
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ name })
+
+    expect(validators).toEqual([
+      new RequiredValidation(name, 'name')
+    ])
   })
 })
