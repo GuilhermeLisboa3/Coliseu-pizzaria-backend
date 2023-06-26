b39c1ba01ec7e4ac162fd47a0f033a26b596294e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 13:26:50 -0300

    feat: ensure DeleteCategoryController call deleteCategory with correct value

--- src/application/controllers/category/delete-category.ts
+++ src/application/controllers/category/delete-category.ts
@@ -1,10 +1,14 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { DeleteCategory } from '@/domain/use-cases/category'
 
 type HttpRequest = { id: string }
 
 export class DeleteCategoryController extends Controller {
+  constructor (private readonly deleteCategory: DeleteCategory) { super() }
+
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
+    await this.deleteCategory({ id })
     return { statusCode: 200, data: null }
   }
 }
