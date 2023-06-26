61ff270c2e5007ff9162aa052594c720e78d8b51
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 15:55:12 -0300

    feat: ensure AddProductUseCase call UUIDGenerator

--- src/domain/use-cases/product/add-product.ts
+++ src/domain/use-cases/product/add-product.ts
@@ -1,18 +1,21 @@
 import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
+import { UUIDGenerator } from '@/domain/contracts/gateways'
 import { FieldInUseError, FieldNotFoundError } from '@/domain/error'
 
 type Setup = (
   productRepository: CheckProductByNameRepository,
-  categoryRepository: CheckCategoryByIdRepository
+  categoryRepository: CheckCategoryByIdRepository,
+  uuid: UUIDGenerator
 ) => AddProduct
 type Input = { name: string, categoryId: string }
 type Output = void
 export type AddProduct = (input: Input) => Promise<Output>
 
-export const addProductUseCase: Setup = (productRepository, categoryRepository) => async ({ name, categoryId }) => {
+export const addProductUseCase: Setup = (productRepository, categoryRepository, uuid) => async ({ name, categoryId }) => {
   const productExists = await productRepository.checkByName({ name })
   if (productExists) throw new FieldInUseError('name')
   const categoryExists = await categoryRepository.checkById({ id: categoryId })
   if (!categoryExists) throw new FieldNotFoundError('categoryId')
+  uuid.generate()
 }
