009521d3f694c91cf5bf4f6e7dd72e72509160a5
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 13:30:55 -0300

    test: ensure DeleteCategoryController return badRequest if deleteCategory return FieldNotFoundError

--- tests/application/controllers/category/delete-category.spec.ts
+++ tests/application/controllers/category/delete-category.spec.ts
@@ -1,6 +1,7 @@
 import { categoryParams } from '@/tests/mocks'
 import { DeleteCategoryController } from '@/application/controllers/category'
 import { Controller } from '@/application/controllers'
+import { FieldNotFoundError } from '@/domain/error'
 
 describe('DeleteCategoryController', () => {
   const { id } = categoryParams
@@ -21,5 +22,13 @@ describe('DeleteCategoryController', () => {
 
     expect(deleteCategory).toHaveBeenCalledWith({ id })
     expect(deleteCategory).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return badRequest if deleteCategory return FieldNotFoundError', async () => {
+    deleteCategory.mockRejectedValueOnce(new FieldNotFoundError('id'))
+    const { statusCode, data } = await sut.handle({ id })
+
+    expect(statusCode).toBe(400)
+    expect(data).toEqual(new FieldNotFoundError('id'))
   })
 })
