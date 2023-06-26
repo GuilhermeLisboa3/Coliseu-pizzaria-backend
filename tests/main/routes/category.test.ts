db967b00ba59173036582e847ff640bb6dd3617b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 11:35:57 -0300

    test: ensure route category return 400 if name already exists

---
+++ tests/main/routes/category.test.ts
@@ -0,0 +1,35 @@
+import { env } from '@/main/config'
+import { categoryParams, accountParams } from '@/tests/mocks'
+import { app } from '@/main/config/app'
+import { prisma } from '@/infra/database/postgres/helpers'
+import { FieldInUseError } from '@/domain/error'
+
+import request from 'supertest'
+import { sign } from 'jsonwebtoken'
+
+describe('Category routes', () => {
+  let token: string
+  const { name } = categoryParams
+  const { email, password, id } = accountParams
+
+  beforeAll(async () => {
+    token = sign({ key: id }, env.secret)
+  })
+
+  beforeEach(async () => {
+    await prisma.user.create({ data: { id, name, email, password, role: 'admin' } })
+  })
+
+  describe('POST /category', () => {
+    it('should return 400 if name already exists', async () => {
+      await prisma.category.create({ data: { name } })
+      const { status, body: { error } } = await request(app)
+        .post('/category')
+        .set({ authorization: `Bearer: ${token}` })
+        .send({ name })
+
+      expect(status).toBe(400)
+      expect(error).toEqual(new FieldInUseError('name').message)
+    })
+  })
+})
