1036a41eab0aa16b9e92b874ad6fe74521b33c04
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 13:27:46 -0300

    test: ensure DeleteCategoryController call deleteCategory with correct value

--- tests/application/controllers/category/delete-category.spec.ts
+++ tests/application/controllers/category/delete-category.spec.ts
@@ -1,14 +1,25 @@
+import { categoryParams } from '@/tests/mocks'
 import { DeleteCategoryController } from '@/application/controllers/category'
 import { Controller } from '@/application/controllers'
 
 describe('DeleteCategoryController', () => {
+  const { id } = categoryParams
+  const deleteCategory = jest.fn()
+
   let sut: DeleteCategoryController
 
   beforeEach(() => {
-    sut = new DeleteCategoryController()
+    sut = new DeleteCategoryController(deleteCategory)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call deleteCategory with correct value', async () => {
+    await sut.handle({ id })
+
+    expect(deleteCategory).toHaveBeenCalledWith({ id })
+    expect(deleteCategory).toHaveBeenCalledTimes(1)
   })
 })
