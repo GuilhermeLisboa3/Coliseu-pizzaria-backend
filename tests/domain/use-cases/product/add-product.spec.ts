d70992d3d596787582fd022c5b7b65d5a80d30a7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 15:41:17 -0300

    test: ensure AddProductUseCase call CheckCategoryByIdRepository with correct value

--- tests/domain/use-cases/product/add-product.spec.ts
+++ tests/domain/use-cases/product/add-product.spec.ts
@@ -1,5 +1,6 @@
-import { productParams } from '@/tests/mocks'
+import { productParams, categoryParams } from '@/tests/mocks'
 import { CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
+import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { AddProduct, addProductUseCase } from '@/domain/use-cases/product'
 
 import { mock } from 'jest-mock-extended'
@@ -7,16 +8,20 @@ import { FieldInUseError } from '@/domain/error'
 
 describe('AddProductUseCase', () => {
   const { name, error } = productParams
+  const { id } = categoryParams
+  const makeParams = { name, categoryId: id }
+
   const productRepository = mock<CheckProductByNameRepository>()
+  const categoryRepository = mock<CheckCategoryByIdRepository>()
 
   let sut: AddProduct
 
   beforeEach(() => {
-    sut = addProductUseCase(productRepository)
+    sut = addProductUseCase(productRepository, categoryRepository)
   })
 
   it('should call CheckProductByNameRepository with correct value', async () => {
-    await sut({ name })
+    await sut(makeParams)
 
     expect(productRepository.checkByName).toHaveBeenCalledWith({ name })
     expect(productRepository.checkByName).toHaveBeenCalledTimes(1)
@@ -25,7 +30,7 @@ describe('AddProductUseCase', () => {
   it('should throw FieldInUseError if CheckProductByNameRepository return true', async () => {
     productRepository.checkByName.mockResolvedValueOnce(true)
 
-    const promise = sut({ name })
+    const promise = sut(makeParams)
 
     await expect(promise).rejects.toThrow(new FieldInUseError('name'))
   })
@@ -33,8 +38,15 @@ describe('AddProductUseCase', () => {
   it('should rethrow if CheckProductByNameRepository throws', async () => {
     productRepository.checkByName.mockRejectedValueOnce(error)
 
-    const promise = sut({ name })
+    const promise = sut(makeParams)
 
     await expect(promise).rejects.toThrow(error)
+  })
+
+  it('should call CheckCategoryByIdRepository with correct value', async () => {
+    await sut(makeParams)
+
+    expect(categoryRepository.checkById).toHaveBeenCalledWith({ id })
+    expect(categoryRepository.checkById).toHaveBeenCalledTimes(1)
   })
 })
