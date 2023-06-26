1e0e8fdab6910f19e3bbfa06a6047bf30884908e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 17:13:54 -0300

    test: ensure UUIDAdapter return a key on success

--- tests/infra/gateways/uuid-adapter.spec.ts
+++ tests/infra/gateways/uuid-adapter.spec.ts
@@ -1,20 +1,32 @@
 import { UUIDAdapter } from '@/infra/gateways'
 
 import { v4 } from 'uuid'
+import faker from 'faker'
 
 jest.mock('uuid')
 
 describe('UUIDAdapter', () => {
   let sut: UUIDAdapter
+  const key = faker.random.word()
+
+  beforeAll(() => {
+    jest.mocked(v4).mockReturnValue(key)
+  })
 
   beforeEach(() => {
     sut = new UUIDAdapter()
   })
 
   it('should call v4', async () => {
-    await sut.generate()
+    sut.generate()
 
     expect(v4).toHaveBeenCalled()
     expect(v4).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return a key on success', async () => {
+    const result = sut.generate()
+
+    expect(result).toBe(key)
   })
 })
