fb7d00278213dde8322fc7d71c7d46392893305a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 12:59:49 -0300

    test: ensure DeleteCategoryUseCase throw FieldNotFoundError if CheckCategoryByIdRepo return false

--- tests/domain/use-cases/category/delete-category.spec.ts
+++ tests/domain/use-cases/category/delete-category.spec.ts
@@ -1,6 +1,7 @@
 import { categoryParams } from '@/tests/mocks'
 import { CheckCategoryByIdRepository } from '@/domain/contracts/database/repositories/category'
 import { DeleteCategory, deleteCategoryUseCase } from '@/domain/use-cases/category'
+import { FieldNotFoundError } from '@/domain/error'
 
 import { mock } from 'jest-mock-extended'
 
@@ -11,6 +12,10 @@ describe('DeleteCategoryUseCase', () => {
 
   const categoryRepository = mock<CheckCategoryByIdRepository>()
 
+  beforeAll(() => {
+    categoryRepository.checkById.mockResolvedValue(true)
+  })
+
   beforeEach(() => {
     sut = deleteCategoryUseCase(categoryRepository)
   })
@@ -20,5 +25,13 @@ describe('DeleteCategoryUseCase', () => {
 
     expect(categoryRepository.checkById).toHaveBeenCalledWith({ id })
     expect(categoryRepository.checkById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw FieldNotFoundError if CheckCategoryByIdRepository return false', async () => {
+    categoryRepository.checkById.mockResolvedValueOnce(false)
+
+    const promise = sut({ id })
+
+    await expect(promise).rejects.toThrow(new FieldNotFoundError('id'))
   })
 })
