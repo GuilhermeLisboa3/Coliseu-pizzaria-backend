bcfb7df4ac81c7e8c5df4d234053cac94a24ba73
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-26 10:58:09 -0300

    feat: ensure CategoryRepository create return category on success

--- src/infra/database/postgres/repositories/category.ts
+++ src/infra/database/postgres/repositories/category.ts
@@ -1,10 +1,16 @@
-import { CheckCategoryByNameRepository } from '@/domain/contracts/database/repositories/category'
+import { CheckCategoryByNameRepository, AddCategoryRepository } from '@/domain/contracts/database/repositories/category'
 import { prisma } from '@/infra/database/postgres/helpers'
 
-export class CategoryRepository implements CheckCategoryByNameRepository {
+export class CategoryRepository implements CheckCategoryByNameRepository, AddCategoryRepository {
   async checkByName ({ name }: CheckCategoryByNameRepository.Input): Promise<CheckCategoryByNameRepository.Output> {
     const isValid = await prisma.category.findFirst({ where: { name } })
 
     return isValid != null
+  }
+
+  async create ({ name }: AddCategoryRepository.Input): Promise<AddCategoryRepository.Output> {
+    const category = await prisma.category.create({ data: { name } })
+
+    return category
   }
 }
