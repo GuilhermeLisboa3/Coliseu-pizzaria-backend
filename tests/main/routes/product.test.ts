f2079b5f664ff4bf7d6e84e6380ab0d6b8be2d23
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-27 15:14:44 -0300

    test: ensure route /product return 400 if name already exists

---
+++ tests/main/routes/product.test.ts
@@ -0,0 +1,37 @@
+import { env } from '@/main/config'
+import { categoryParams, accountParams, productParams } from '@/tests/mocks'
+import { app } from '@/main/config/app'
+import { prisma } from '@/infra/database/postgres/helpers'
+import { FieldInUseError } from '@/domain/error'
+
+import request from 'supertest'
+import { sign } from 'jsonwebtoken'
+
+describe('Product routes', () => {
+  let token: string
+  const { name } = categoryParams
+  const { email, password, id } = accountParams
+  const { description, picture, price, available } = productParams
+
+  beforeAll(async () => {
+    token = sign({ key: id }, env.secret)
+  })
+
+  beforeEach(async () => {
+    await prisma.user.create({ data: { id, name, email, password, role: 'admin' } })
+    await prisma.category.create({ data: { id, name } })
+  })
+
+  describe('POST /product', () => {
+    it('should return 400 if name already exists', async () => {
+      await prisma.product.create({ data: { name, description, picture, price, available, category_id: id } })
+      const { status, body: { error } } = await request(app)
+        .post('/product')
+        .set({ authorization: `Bearer: ${token}` })
+        .send({ categoryId: id, name, description, price })
+
+      expect(status).toBe(400)
+      expect(error).toEqual(new FieldInUseError('name').message)
+    })
+  })
+})
