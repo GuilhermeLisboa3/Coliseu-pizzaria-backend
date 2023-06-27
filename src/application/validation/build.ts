1a41b50658c2a0778ca313770960166144e4b823
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-27 13:26:38 -0300

    feat: ensure ValidationBuilder return a Image validation if image() is call

--- src/application/validation/build.ts
+++ src/application/validation/build.ts
@@ -1,4 +1,4 @@
-import { EmailValidation, MinSizeValidation, RequiredValidation, Validator } from '@/application/validation'
+import { AllowedMimeTypesValidation, EmailValidation, Extension, MaxFileSizeValidation, MinSizeValidation, RequiredValidation, Validator } from '@/application/validation'
 
 export class ValidationBuilder {
   private constructor (
@@ -23,6 +23,12 @@ export class ValidationBuilder {
 
   min (length: number): ValidationBuilder {
     this.validators.push(new MinSizeValidation(this.value, length))
+    return this
+  }
+
+  image ({ AllowedMimeTypes, maxSizeInMb }: { AllowedMimeTypes: Extension[], maxSizeInMb: number }): ValidationBuilder {
+    if (this.value?.mimeType) this.validators.push(new AllowedMimeTypesValidation(AllowedMimeTypes, this.value.mimeType))
+    if (this.value?.buffer) this.validators.push(new MaxFileSizeValidation(maxSizeInMb, this.value.buffer))
     return this
   }
 
