05de08460912897f122bd96991d967347f700231
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-27 13:48:04 -0300

    test: ensure AddProductController build Validators correctly

--- tests/application/controllers/product/add-product.spec.ts
+++ tests/application/controllers/product/add-product.spec.ts
@@ -1,7 +1,11 @@
+import { productParams } from '@/tests/mocks'
 import { AddProductController } from '@/application/controllers/product'
 import { Controller } from '@/application/controllers'
+import { AllowedMimeTypesValidation, MaxFileSizeValidation, RequiredValidation } from '@/application/validation'
 
 describe('AddProductController', () => {
+  const { name, description, price, file, id } = productParams
+  const makeParams = { categoryId: id, name, description, price, file }
   let sut: AddProductController
 
   beforeEach(() => {
@@ -10,5 +14,18 @@ describe('AddProductController', () => {
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should build Validators correctly', async () => {
+    const validators = sut.buildValidators(makeParams)
+
+    expect(validators).toEqual([
+      new RequiredValidation(name, 'name'),
+      new RequiredValidation(id, 'categoryId'),
+      new RequiredValidation(description, 'description'),
+      new RequiredValidation(price, 'price'),
+      new AllowedMimeTypesValidation(['png', 'jpg'], file.mimeType),
+      new MaxFileSizeValidation(10, file.buffer)
+    ])
   })
 })
