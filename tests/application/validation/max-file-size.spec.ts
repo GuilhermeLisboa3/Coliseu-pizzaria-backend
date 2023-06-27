f03ee209cc66ad7ad0b081b0ad00326e47c832ad
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-27 13:13:14 -0300

    test: ensure MaxFileSizeValidation return undefined if value is valid

--- tests/application/validation/max-file-size.spec.ts
+++ tests/application/validation/max-file-size.spec.ts
@@ -10,4 +10,22 @@ describe('MaxFileSizeValidation', () => {
 
     expect(error).toEqual(new MaxFileSizeError(5))
   })
+
+  it('should return undefined if value is valid', () => {
+    const buffer = Buffer.from(new ArrayBuffer(4 * 1024 * 1024))
+    const sut = new MaxFileSizeValidation(5, buffer)
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  it('should return undefined if value is valid', () => {
+    const buffer = Buffer.from(new ArrayBuffer(5 * 1024 * 1024))
+    const sut = new MaxFileSizeValidation(5, buffer)
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
 })
