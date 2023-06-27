0276532a2df9283d3ba8085898d3f3a1fb9a0e99
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-27 14:48:52 -0300

    test: ensure MulterAdapter call next on success

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -82,4 +82,11 @@ describe('MulterAdapter', () => {
 
     expect(req.locals).toEqual({ [key]: value, file: { buffer: req.file?.buffer, mimeType: req.file?.mimetype } })
   })
+
+  it('should call next on success', () => {
+    sut(req, res, next)
+
+    expect(next).toHaveBeenCalledWith()
+    expect(next).toHaveBeenCalledTimes(1)
+  })
 })
