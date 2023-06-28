3216c692d88641f5f4cf4608c62a1e21268a7c78
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-28 11:30:43 -0300

    test: ensure updateProductUseCase throw FieldNotFoundError if LoadProductRepository return null

---
+++ tests/domain/use-cases/product/update-product.spec.ts
@@ -0,0 +1,39 @@
+import { productParams, categoryParams } from '@/tests/mocks'
+import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
+import { UpdateProduct, updateProductUseCase } from '@/domain/use-cases/product'
+
+import { mock } from 'jest-mock-extended'
+import { FieldNotFoundError } from '@/domain/error'
+
+describe('updateProductUseCase', () => {
+  const { name, file, description, price, available, picture } = productParams
+  const { id } = categoryParams
+  const makeParams = { id, name, categoryId: id, description, price, file }
+
+  const productRepository = mock<LoadProductRepository>()
+
+  let sut: UpdateProduct
+
+  beforeAll(() => {
+    productRepository.load.mockResolvedValue({ id, name, description, price, categoryId: id, available, picture })
+  })
+
+  beforeEach(() => {
+    sut = updateProductUseCase(productRepository)
+  })
+
+  it('should call LoadProductRepository with correct id', async () => {
+    await sut(makeParams)
+
+    expect(productRepository.load).toHaveBeenCalledWith({ id })
+    expect(productRepository.load).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw FieldNotFoundError if LoadProductRepository return null', async () => {
+    productRepository.load.mockResolvedValueOnce(null)
+
+    const promise = sut(makeParams)
+
+    await expect(promise).rejects.toThrow(new FieldNotFoundError('id'))
+  })
+})
