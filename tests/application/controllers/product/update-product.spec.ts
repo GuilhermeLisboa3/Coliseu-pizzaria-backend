8af3713c86f6fbe32e554b84a02eabe989fd5109
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-28 15:26:21 -0300

    test: ensure UpdateProductController build Validators correctly

--- tests/application/controllers/product/update-product.spec.ts
+++ tests/application/controllers/product/update-product.spec.ts
@@ -1,7 +1,10 @@
+import { productParams } from '@/tests/mocks'
 import { UpdateProductController } from '@/application/controllers/product'
 import { Controller } from '@/application/controllers'
+import { AllowedMimeTypesValidation, MaxFileSizeValidation, RequiredValidation } from '@/application/validation'
 
 describe('UpdateProductController', () => {
+  const { id, name, file } = productParams
   let sut: UpdateProductController
 
   beforeEach(() => {
@@ -10,5 +13,15 @@ describe('UpdateProductController', () => {
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ id, name, file })
+
+    expect(validators).toEqual([
+      new RequiredValidation(id, 'id'),
+      new AllowedMimeTypesValidation(['png', 'jpg'], file.mimeType),
+      new MaxFileSizeValidation(10, file.buffer)
+    ])
   })
 })
