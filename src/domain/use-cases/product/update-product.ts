3b8fbac8da9bd8b62657de278c52cc9c0743d3c6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-28 11:35:20 -0300

    feat: ensure updateProductUseCase call CheckProductByNameRepository with correct value

--- src/domain/use-cases/product/update-product.ts
+++ src/domain/use-cases/product/update-product.ts
@@ -1,8 +1,8 @@
-import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
+import { LoadProductRepository, CheckProductByNameRepository } from '@/domain/contracts/database/repositories/product'
 import { FieldNotFoundError } from '@/domain/error'
 
 type Setup = (
-  productRepository: LoadProductRepository
+  productRepository: LoadProductRepository & CheckProductByNameRepository
 ) => UpdateProduct
 type Input = { id: string, categoryId?: string, name?: string, description?: string, price?: number, file?: { buffer: Buffer, mimeType: string }, available?: boolean }
 type Output = void
@@ -11,4 +11,7 @@ export type UpdateProduct = (input: Input) => Promise<Output>
 export const updateProductUseCase: Setup = (productRepository) => async ({ name, categoryId, description, price, file, id, available }) => {
   const product = await productRepository.load({ id })
   if (!product) throw new FieldNotFoundError('id')
+  if (name) {
+    await productRepository.checkByName({ name })
+  }
 }
