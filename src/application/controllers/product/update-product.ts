25dc7abd11c48f41424402714dd1b8b736cf0687
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-28 15:00:29 -0300

    feat: ensure UpdateProductController build Validators correctly

--- src/application/controllers/product/update-product.ts
+++ src/application/controllers/product/update-product.ts
@@ -1,10 +1,18 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { Validator, ValidationBuilder as Builder } from '@/application/validation'
 
 type HttpRequest = { id: string, categoryId?: string, name?: string, description?: string, price?: number, file?: { buffer: Buffer, mimeType: string }, available?: boolean }
 
 export class UpdateProductController extends Controller {
   async perform ({ categoryId, description, name, price, file, id, available }: HttpRequest): Promise<HttpResponse> {
     return { statusCode: 200, data: null }
+  }
+
+  buildValidators ({ id, file }: HttpRequest): Validator[] {
+    return [
+      ...Builder.of(id, 'id').required().build(),
+      ...Builder.of(file, 'file').image({ AllowedMimeTypes: ['png', 'jpg'], maxSizeInMb: 10 }).build()
+    ]
   }
 }
