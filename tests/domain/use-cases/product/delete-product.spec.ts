03870580835d07e91659d54c866d1612984e841e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-29 10:58:08 -0300

    test: ensure deleteProductUseCase throw FieldNotFoundError if LoadProductRepository return null

--- tests/domain/use-cases/product/delete-product.spec.ts
+++ tests/domain/use-cases/product/delete-product.spec.ts
@@ -3,14 +3,19 @@ import { LoadProductRepository } from '@/domain/contracts/database/repositories/
 import { DeleteProduct, deleteProductUseCase } from '@/domain/use-cases/product'
 
 import { mock } from 'jest-mock-extended'
+import { FieldNotFoundError } from '@/domain/error'
 
 describe('deleteProductUseCase', () => {
-  const { id } = productParams
+  const { id, name, description, price, available, picture } = productParams
   const makeParams = { id }
 
   const productRepository = mock<LoadProductRepository>()
 
   let sut: DeleteProduct
+
+  beforeAll(() => {
+    productRepository.load.mockResolvedValue({ id, name, description, price, categoryId: id, available, picture })
+  })
 
   beforeEach(() => {
     sut = deleteProductUseCase(productRepository)
@@ -21,5 +26,13 @@ describe('deleteProductUseCase', () => {
 
     expect(productRepository.load).toHaveBeenCalledWith({ id })
     expect(productRepository.load).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw FieldNotFoundError if LoadProductRepository return null', async () => {
+    productRepository.load.mockResolvedValueOnce(null)
+
+    const promise = sut(makeParams)
+
+    await expect(promise).rejects.toThrow(new FieldNotFoundError('id'))
   })
 })
