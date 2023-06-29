acd3f1972be690a8abdca360ab0bf2a1420dbd7d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-29 12:23:59 -0300

    feat: ensure DeleteProductController call deleteProduct with correct values

--- src/application/controllers/product/delete-product.ts
+++ src/application/controllers/product/delete-product.ts
@@ -1,10 +1,14 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { DeleteProduct } from '@/domain/use-cases/product'
 
 type HttpRequest = { id: string }
 
 export class DeleteProductController extends Controller {
+  constructor (private readonly deleteProduct: DeleteProduct) { super() }
+
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
+    await this.deleteProduct({ id })
     return { statusCode: 200, data: null }
   }
 }
