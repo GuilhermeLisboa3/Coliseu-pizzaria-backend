a90ceb3ebd5fdfacbd486c252380ca43856d64fa
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-29 12:28:13 -0300

    test: ensure DeleteProductController return badRequest if updateProduct return FieldNotFoundError

--- tests/application/controllers/product/delete-product.spec.ts
+++ tests/application/controllers/product/delete-product.spec.ts
@@ -1,6 +1,7 @@
 import { productParams } from '@/tests/mocks'
 import { DeleteProductController } from '@/application/controllers/product'
 import { Controller } from '@/application/controllers'
+import { FieldNotFoundError } from '@/domain/error'
 
 describe('DeleteProductController', () => {
   const { id } = productParams
@@ -20,5 +21,13 @@ describe('DeleteProductController', () => {
 
     expect(deleteProduct).toHaveBeenCalledWith({ id })
     expect(deleteProduct).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return badRequest if updateProduct return FieldNotFoundError', async () => {
+    deleteProduct.mockRejectedValueOnce(new FieldNotFoundError('id'))
+    const { statusCode, data } = await sut.handle({ id })
+
+    expect(statusCode).toBe(400)
+    expect(data).toEqual(new FieldNotFoundError('id'))
   })
 })
