d4cf3d5fe0e1c97992c2c7015aa8f2d873f3e6d2
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-29 12:46:32 -0300

    feat: ensure route /product/:id return 400 if product not exists

--- src/main/routes/product.ts
+++ src/main/routes/product.ts
@@ -1,10 +1,11 @@
 import { expressRouterAdapter as adapt, multerAdapter } from '@/main/adapters'
 import { authAdmin } from '@/main/middlewares'
-import { makeAddProductController, makeUpdateProductController } from '@/main/factories/application/controllers/product'
+import { makeAddProductController, makeUpdateProductController, makeDeleteProductController } from '@/main/factories/application/controllers/product'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/product', authAdmin, multerAdapter, adapt(makeAddProductController()))
   router.put('/product/:id', authAdmin, multerAdapter, adapt(makeUpdateProductController()))
+  router.delete('/product/:id', authAdmin, adapt(makeDeleteProductController()))
 }
