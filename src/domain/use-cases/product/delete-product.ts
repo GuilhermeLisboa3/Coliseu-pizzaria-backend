37676bd04b399d98c56d1c31cafa8f6ba30da30f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-29 10:57:17 -0300

    feat: ensure deleteProductUseCase throw FieldNotFoundError if LoadProductRepository return null

--- src/domain/use-cases/product/delete-product.ts
+++ src/domain/use-cases/product/delete-product.ts
@@ -1,4 +1,5 @@
 import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
+import { FieldNotFoundError } from '@/domain/error'
 
 type Setup = (productRepository: LoadProductRepository) => DeleteProduct
 type Input = { id: string }
@@ -6,5 +7,6 @@ type Output = void
 export type DeleteProduct = (input: Input) => Promise<Output>
 
 export const deleteProductUseCase: Setup = (productRepository) => async ({ id }) => {
-  await productRepository.load({ id })
+  const product = await productRepository.load({ id })
+  if (!product) throw new FieldNotFoundError('id')
 }
