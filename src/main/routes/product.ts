ebc699e942d6296559d27c98b5dd02bf998828a7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-29 08:06:18 -0300

    feat: ensure route /product/:id return 400 if name already exists

--- src/main/routes/product.ts
+++ src/main/routes/product.ts
@@ -1,9 +1,10 @@
 import { expressRouterAdapter as adapt, multerAdapter } from '@/main/adapters'
 import { authAdmin } from '@/main/middlewares'
-import { makeAddProductController } from '@/main/factories/application/controllers/product'
+import { makeAddProductController, makeUpdateProductController } from '@/main/factories/application/controllers/product'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/product', authAdmin, multerAdapter, adapt(makeAddProductController()))
+  router.put('/product/:id', authAdmin, multerAdapter, adapt(makeUpdateProductController()))
 }
