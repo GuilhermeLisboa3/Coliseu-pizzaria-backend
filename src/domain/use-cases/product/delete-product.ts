bc4fb83b235a9809cb8ef496b8fb3e12463cbb05
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-29 11:03:10 -0300

    feat: ensure deleteProductUseCase call DeleteFile if product has picture

--- src/domain/use-cases/product/delete-product.ts
+++ src/domain/use-cases/product/delete-product.ts
@@ -1,12 +1,14 @@
 import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
+import { DeleteFile } from '@/domain/contracts/gateways'
 import { FieldNotFoundError } from '@/domain/error'
 
-type Setup = (productRepository: LoadProductRepository) => DeleteProduct
+type Setup = (productRepository: LoadProductRepository, fileStorage: DeleteFile) => DeleteProduct
 type Input = { id: string }
 type Output = void
 export type DeleteProduct = (input: Input) => Promise<Output>
 
-export const deleteProductUseCase: Setup = (productRepository) => async ({ id }) => {
+export const deleteProductUseCase: Setup = (productRepository, fileStorage) => async ({ id }) => {
   const product = await productRepository.load({ id })
   if (!product) throw new FieldNotFoundError('id')
+  if (product.picture) await fileStorage.delete({ fileName: product.picture })
 }
