0a30c741f7da9f009e1da6946b52c7926f005528
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-29 12:27:04 -0300

    test: ensure DeleteProductController call deleteProduct with correct values

--- tests/application/controllers/product/delete-product.spec.ts
+++ tests/application/controllers/product/delete-product.spec.ts
@@ -1,14 +1,24 @@
+import { productParams } from '@/tests/mocks'
 import { DeleteProductController } from '@/application/controllers/product'
 import { Controller } from '@/application/controllers'
 
 describe('DeleteProductController', () => {
+  const { id } = productParams
+  const deleteProduct = jest.fn()
   let sut: DeleteProductController
 
   beforeEach(() => {
-    sut = new DeleteProductController()
+    sut = new DeleteProductController(deleteProduct)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call deleteProduct with correct values', async () => {
+    await sut.handle({ id })
+
+    expect(deleteProduct).toHaveBeenCalledWith({ id })
+    expect(deleteProduct).toHaveBeenCalledTimes(1)
   })
 })
