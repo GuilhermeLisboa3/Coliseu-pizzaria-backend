aa9f7e39fa354f5eb9b75b162652631a472ded14
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-30 16:44:21 -0300

    test: ensure AxiosHttpClient return data on success

--- tests/infra/gateways/axios-adapter.spec.ts
+++ tests/infra/gateways/axios-adapter.spec.ts
@@ -8,10 +8,13 @@ jest.mock('axios')
 describe('AxiosHttpClient', () => {
   let sut: AxiosHttpClient
   let url: string
+  let data: string
   const fakeAxios = axios as jest.Mocked<typeof axios>
 
   beforeAll(() => {
+    data = faker.random.objectElement()
     url = faker.internet.url()
+    fakeAxios.get.mockResolvedValue(data)
   })
 
   beforeEach(() => {
@@ -23,5 +26,11 @@ describe('AxiosHttpClient', () => {
 
     expect(fakeAxios.get).toHaveBeenCalledWith(url)
     expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return data on success', async () => {
+    const result = await sut.get({ url })
+
+    expect(result).toEqual(data)
   })
 })
