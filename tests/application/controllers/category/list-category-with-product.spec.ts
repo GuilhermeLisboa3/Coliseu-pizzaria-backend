c18b31c0c8656aef6d9d18eed8018504e1eb1a2b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-30 14:32:56 -0300

    test: ensure ListCategoryWithProductController call listCategoryWithProduct

--- tests/application/controllers/category/list-category-with-product.spec.ts
+++ tests/application/controllers/category/list-category-with-product.spec.ts
@@ -3,12 +3,20 @@ import { Controller } from '@/application/controllers'
 
 describe('ListCategoryWithProductController', () => {
   let sut: ListCategoryWithProductController
+  const listCategoryWithProduct = jest.fn()
 
   beforeEach(() => {
-    sut = new ListCategoryWithProductController()
+    sut = new ListCategoryWithProductController(listCategoryWithProduct)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call listCategoryWithProduct', async () => {
+    await sut.handle()
+
+    expect(listCategoryWithProduct).toHaveBeenCalled()
+    expect(listCategoryWithProduct).toHaveBeenCalledTimes(1)
   })
 })
