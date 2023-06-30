7b9de44922ac6b9b78c18825d3388788affcae77
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-30 17:32:05 -0300

    feat: ensure ZipCodeApi return andress on success

--- src/infra/gateways/zipcode-api.ts
+++ src/infra/gateways/zipcode-api.ts
@@ -4,9 +4,10 @@ import { HttpGetClient } from '@/infra/gateways/contracts/gateways'
 export class ZipCodeApi {
   constructor (private readonly httpGetClient: HttpGetClient) {}
 
-  async search ({ zipCode }: SearchAddressByZipCode.Input): Promise<void> {
+  async search ({ zipCode }: SearchAddressByZipCode.Input): Promise<SearchAddressByZipCode.Output> {
     const url = `https://brasilapi.com.br/api/cep/v2/${zipCode}`
-    const { status } = await this.httpGetClient.get({ url })
+    const { status, data: { neighborhood, street } } = await this.httpGetClient.get({ url })
     if (status !== 200) return undefined
+    return { neighborhood, street }
   }
 }
