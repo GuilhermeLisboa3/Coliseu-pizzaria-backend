20e0d27136384baf6e7cb9dc015b217cecbf6d45
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-30 14:55:33 -0300

    feat: ensure route /categories return 200 on success

--- src/main/factories/application/controllers/category/delete-category.ts
+++ src/main/factories/application/controllers/category/delete-category.ts
@@ -2,6 +2,6 @@ import { Controller } from '@/application/controllers'
 import { DeleteCategoryController } from '@/application/controllers/category'
 import { makeDeleteCategory } from '@/main/factories/domain/use-cases/category'
 
-export const makeDeleteCategoryControllerr = (): Controller => {
+export const makeDeleteCategoryController = (): Controller => {
   return new DeleteCategoryController(makeDeleteCategory())
 }
--- src/main/routes/category.ts
+++ src/main/routes/category.ts
@@ -1,10 +1,11 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
-import { authAdmin } from '@/main/middlewares'
-import { makeAddCategoryController, makeDeleteCategoryControllerr } from '@/main/factories/application/controllers/category'
+import { authAdmin, auth } from '@/main/middlewares'
+import { makeAddCategoryController, makeDeleteCategoryController, makeListCategoryWithProductController } from '@/main/factories/application/controllers/category'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/category', authAdmin, adapt(makeAddCategoryController()))
-  router.delete('/category/:id', authAdmin, adapt(makeDeleteCategoryControllerr()))
+  router.get('/categories', auth, adapt(makeListCategoryWithProductController()))
+  router.delete('/category/:id', authAdmin, adapt(makeDeleteCategoryController()))
 }
