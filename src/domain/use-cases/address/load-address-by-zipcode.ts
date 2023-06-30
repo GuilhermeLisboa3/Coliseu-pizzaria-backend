1d85fc401050175e9dbf93b1a5de4eee5165b6bd
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-30 16:31:31 -0300

    feat: ensure loadAddressByZipCodeUseCase return address on success

--- src/domain/use-cases/address/load-address-by-zipcode.ts
+++ src/domain/use-cases/address/load-address-by-zipcode.ts
@@ -3,10 +3,11 @@ import { FieldNotFoundError } from '@/domain/error'
 
 type Setup = (searchAddressByZipCode: SearchAddressByZipCode) => LoadAddressByZipCode
 type Input = { zipCode: string }
-type Output = void
+type Output = { neighborhood: string, street: string }
 export type LoadAddressByZipCode = (input: Input) => Promise<Output>
 
 export const loadAddressByZipCodeUseCase: Setup = searchAddressByZipCode => async ({ zipCode }) => {
   const address = await searchAddressByZipCode.search({ zipCode })
   if (!address) throw new FieldNotFoundError('zipCode')
+  return address
 }
