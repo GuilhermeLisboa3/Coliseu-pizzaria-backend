9ed02e9cabdb11c01e00fadc11040aee3ec6c573
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-30 16:41:48 -0300

    feat: ensure AxiosHttpClient call get with correct values

---
+++ src/infra/gateways/axios-adapter.ts
@@ -0,0 +1,11 @@
+import { HttpGetClient } from '@/infra/gateways/contracts/gateways'
+
+import axios from 'axios'
+
+type Input = HttpGetClient.Input
+
+export class AxiosHttpClient implements HttpGetClient {
+  async get ({ url }: Input): Promise<any> {
+    return await axios.get(url)
+  }
+}
