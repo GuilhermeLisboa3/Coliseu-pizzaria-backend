1d47cf293b09da64250f958cb7db47eb8a2a019a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-06-30 14:34:54 -0300

    feat: ensure ListCategoryWithProductController return ok on success

--- src/application/controllers/category/list-category-with-product.ts
+++ src/application/controllers/category/list-category-with-product.ts
@@ -1,12 +1,12 @@
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '@/application/helpers'
+import { HttpResponse, ok } from '@/application/helpers'
 import { ListCategoryWithProduct } from '@/domain/use-cases/category'
 
 export class ListCategoryWithProductController extends Controller {
   constructor (private readonly listCategoryWithProduct: ListCategoryWithProduct) { super() }
 
   async perform (): Promise<HttpResponse> {
-    await this.listCategoryWithProduct()
-    return { statusCode: 200, data: null }
+    const category = await this.listCategoryWithProduct()
+    return ok(category)
   }
 }
