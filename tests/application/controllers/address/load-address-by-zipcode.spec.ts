9d1c95a16d44f705cfa6aa60281f039cced76c04
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 11:35:12 -0300

    test: ensure LoadAddressByZipCodeController build Validators correctly

--- tests/application/controllers/address/load-address-by-zipcode.spec.ts
+++ tests/application/controllers/address/load-address-by-zipcode.spec.ts
@@ -1,8 +1,11 @@
 import { LoadAddressByZipCodeController } from '@/application/controllers/address'
 import { Controller } from '@/application/controllers'
+import { addressParams } from '@/tests/mocks'
+import { RequiredValidation } from '@/application/validation'
 
 describe('LoadAddressByZipCodeController', () => {
   let sut: LoadAddressByZipCodeController
+  const { zipCode } = addressParams
 
   beforeEach(() => {
     sut = new LoadAddressByZipCodeController()
@@ -10,5 +13,13 @@ describe('LoadAddressByZipCodeController', () => {
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ zipCode })
+
+    expect(validators).toEqual([
+      new RequiredValidation(zipCode, 'zipCode')
+    ])
   })
 })
