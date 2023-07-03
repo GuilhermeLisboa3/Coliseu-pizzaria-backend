760d56c6fb5856d44003076f1a699691c698c12c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 15:46:53 -0300

    test: ensure AddAddressController build Validators correctly

--- tests/application/controllers/address/add-address.spec.ts
+++ tests/application/controllers/address/add-address.spec.ts
@@ -1,8 +1,11 @@
 import { AddAddressController } from '@/application/controllers/address'
 import { Controller } from '@/application/controllers'
+import { addressParams } from '@/tests/mocks'
+import { RequiredValidation } from '@/application/validation'
 
 describe('AddAddressController', () => {
   let sut: AddAddressController
+  const { neighborhood, complement, number, street, surname, zipCode, id } = addressParams
 
   beforeEach(() => {
     sut = new AddAddressController()
@@ -10,5 +13,17 @@ describe('AddAddressController', () => {
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ neighborhood, complement, number, street, surname, zipCode, accountId: id })
+
+    expect(validators).toEqual([
+      new RequiredValidation(zipCode, 'zipCode'),
+      new RequiredValidation(neighborhood, 'neighborhood'),
+      new RequiredValidation(number, 'number'),
+      new RequiredValidation(street, 'street'),
+      new RequiredValidation(surname, 'surname')
+    ])
   })
 })
