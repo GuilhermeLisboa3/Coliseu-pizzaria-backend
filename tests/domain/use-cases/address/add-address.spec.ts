f0da265bb082115b0eddd071bdaa68b472bf297a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 12:36:27 -0300

    test: ensure addAddressUseCase throw FieldNotFoundError if SearchAddressByZipCode return undefined

--- tests/domain/use-cases/address/add-address.spec.ts
+++ tests/domain/use-cases/address/add-address.spec.ts
@@ -3,6 +3,7 @@ import { SearchAddressByZipCode } from '@/domain/contracts/gateways'
 import { addAddressUseCase, AddAddress } from '@/domain/use-cases/address'
 
 import { mock } from 'jest-mock-extended'
+import { FieldNotFoundError } from '@/domain/error'
 
 describe('addAddressUseCase', () => {
   let sut: AddAddress
@@ -10,6 +11,10 @@ describe('addAddressUseCase', () => {
   const { zipCode, neighborhood, street, complement, id, number, surname } = addressParams
 
   const searchAddressByZipCode = mock<SearchAddressByZipCode>()
+
+  beforeAll(() => {
+    searchAddressByZipCode.search.mockResolvedValue({ neighborhood, street })
+  })
 
   beforeEach(() => {
     sut = addAddressUseCase(searchAddressByZipCode)
@@ -20,5 +25,13 @@ describe('addAddressUseCase', () => {
 
     expect(searchAddressByZipCode.search).toHaveBeenCalledWith({ zipCode })
     expect(searchAddressByZipCode.search).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw FieldNotFoundError if SearchAddressByZipCode return undefined', async () => {
+    searchAddressByZipCode.search.mockResolvedValueOnce(undefined)
+
+    const promise = sut({ zipCode, neighborhood, street, number, surname, complement, accountId: id })
+
+    await expect(promise).rejects.toThrow(new FieldNotFoundError('zipCode'))
   })
 })
