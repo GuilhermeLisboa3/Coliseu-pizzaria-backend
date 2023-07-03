d259b2894bb40c1026e92993677af152a666cd57
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 15:48:09 -0300

    feat: ensure AddAddressController call addAddress with correct values

--- src/application/controllers/address/add-address.ts
+++ src/application/controllers/address/add-address.ts
@@ -1,11 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
 import { Validator, ValidationBuilder as Builder } from '@/application/validation'
+import { AddAddress } from '@/domain/use-cases/address'
 
 type HttpRequest = { accountId: string, surname: string, zipCode: string, neighborhood: string, street: string, number: number, complement?: string }
 
 export class AddAddressController extends Controller {
+  constructor (private readonly addAddress: AddAddress) { super() }
+
   async perform (input: HttpRequest): Promise<HttpResponse> {
+    await this.addAddress({ ...input })
     return { data: null, statusCode: 200 }
   }
 
