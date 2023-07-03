1a8042287ec2493540c0da24080f5b874b597d89
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 16:43:21 -0300

    feat: ensure ListAddressesController return ok on success

--- src/application/controllers/address/list-addresses.ts
+++ src/application/controllers/address/list-addresses.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '@/application/helpers'
+import { HttpResponse, ok } from '@/application/helpers'
 import { ListAddresses } from '@/domain/use-cases/address'
 
 type HttpRequest = { accountId: string }
@@ -8,7 +8,7 @@ export class ListAddressesController extends Controller {
   constructor (private readonly listAddresses: ListAddresses) { super() }
 
   async perform ({ accountId }: HttpRequest): Promise<HttpResponse> {
-    await this.listAddresses({ accountId })
-    return { statusCode: 200, data: null }
+    const addresses = await this.listAddresses({ accountId })
+    return ok(addresses)
   }
 }
