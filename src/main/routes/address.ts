a50cc080f935ede69ebf656881a57a2a22e4b3e9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 16:15:52 -0300

    feat: ensure route post /addresss return 400 if zipCode not exists

--- src/main/routes/address.ts
+++ src/main/routes/address.ts
@@ -1,9 +1,10 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
 import { auth } from '@/main/middlewares'
-import { makeLoadAddressByZipCodeController } from '@/main/factories/application/controllers/address'
+import { makeLoadAddressByZipCodeController, makeAddAddressController } from '@/main/factories/application/controllers/address'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.get('/address', auth, adapt(makeLoadAddressByZipCodeController()))
+  router.post('/address', auth, adapt(makeAddAddressController()))
 }
