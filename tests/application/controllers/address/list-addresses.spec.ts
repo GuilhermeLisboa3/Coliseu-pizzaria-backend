6e4db2dbc9d27aa81b559fd64b012b281ca9afc2
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 16:44:07 -0300

    test: ensure ListAddressesController return ok on success

--- tests/application/controllers/address/list-addresses.spec.ts
+++ tests/application/controllers/address/list-addresses.spec.ts
@@ -1,11 +1,16 @@
 import { ListAddressesController } from '@/application/controllers/address'
-import { accountParams } from '@/tests/mocks'
+import { accountParams, addressParams } from '@/tests/mocks'
 import { Controller } from '@/application/controllers'
 
 describe('ListAddressesController', () => {
   const { id: accountId } = accountParams
+  const { active, complement, id, neighborhood, number, street, surname, zipCode } = addressParams
   const listAddresses = jest.fn()
   let sut: ListAddressesController
+
+  beforeAll(() => {
+    listAddresses.mockResolvedValue([{ active, complement, id, neighborhood, number, street, surname, zipCode }])
+  })
 
   beforeEach(() => {
     sut = new ListAddressesController(listAddresses)
@@ -20,5 +25,12 @@ describe('ListAddressesController', () => {
 
     expect(listAddresses).toHaveBeenCalledWith({ accountId })
     expect(listAddresses).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return ok on success', async () => {
+    const { statusCode, data } = await sut.handle({ accountId })
+
+    expect(statusCode).toBe(200)
+    expect(data).toEqual([{ active, complement, id, neighborhood, number, street, surname, zipCode }])
   })
 })
