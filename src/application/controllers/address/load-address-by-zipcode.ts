feaf17a4bc1a5c586db7c4baceec82724326691d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 11:36:35 -0300

    feat: ensure LoadAddressByZipCodeController call loadAddressByZipCode with correct values

--- src/application/controllers/address/load-address-by-zipcode.ts
+++ src/application/controllers/address/load-address-by-zipcode.ts
@@ -1,11 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
 import { Validator, ValidationBuilder as Builder } from '@/application/validation'
+import { LoadAddressByZipCode } from '@/domain/use-cases/address'
 
 type HttpRequest = { zipCode: string }
 
 export class LoadAddressByZipCodeController extends Controller {
+  constructor (private readonly loadAddressByZipCode: LoadAddressByZipCode) { super() }
+
   async perform ({ zipCode }: HttpRequest): Promise<HttpResponse> {
+    await this.loadAddressByZipCode({ zipCode })
     return { statusCode: 200, data: null }
   }
 
