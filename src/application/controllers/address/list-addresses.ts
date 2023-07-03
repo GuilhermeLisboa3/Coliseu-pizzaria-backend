c6f87624afded935b4646eb61370abe2fa70730e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 16:39:06 -0300

    feat: ensure ListAddressesController call listAddresses with correct values

--- src/application/controllers/address/list-addresses.ts
+++ src/application/controllers/address/list-addresses.ts
@@ -1,10 +1,14 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { ListAddresses } from '@/domain/use-cases/address'
 
 type HttpRequest = { accountId: string }
 
 export class ListAddressesController extends Controller {
-  async perform (input: HttpRequest): Promise<HttpResponse> {
+  constructor (private readonly listAddresses: ListAddresses) { super() }
+
+  async perform ({ accountId }: HttpRequest): Promise<HttpResponse> {
+    await this.listAddresses({ accountId })
     return { statusCode: 200, data: null }
   }
 }
