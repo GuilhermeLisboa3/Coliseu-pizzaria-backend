959466b358aa4ae8a1476849a82a7ab365b0f3be
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 14:36:21 -0300

    test: ensure AddressRepository list return address list

--- tests/infra/database/postgres/repositories/address.spec.ts
+++ tests/infra/database/postgres/repositories/address.spec.ts
@@ -1,22 +1,31 @@
 import { AddressRepository } from '@/infra/database/postgres/repositories'
-import { accountParams } from '@/tests/mocks'
+import { accountParams, addressParams } from '@/tests/mocks'
 import { prisma } from '@/infra/database/postgres/helpers'
 
 describe('AddressRepository', () => {
   let sut: AddressRepository
   const { email, name, password, id } = accountParams
+  const { active, complement, neighborhood, number, street, surname, zipCode } = addressParams
 
   beforeEach(async () => {
     await prisma.$queryRaw`DELETE FROM users`
+    await prisma.$queryRaw`DELETE FROM addresses`
     await prisma.user.create({ data: { id, name, email, password } })
     sut = new AddressRepository()
   })
 
   describe('list()', () => {
-    it('should return array empty if address not exists', async () => {
+    it('should return list empty if address not exists', async () => {
       const result = await sut.list({ accountId: id })
 
       expect(result).toEqual([])
+    })
+
+    it('should return address list', async () => {
+      await prisma.address.create({ data: { id, active, complement, neighborhood, number, street, surname, zipCode, user_id: id } })
+      const result = await sut.list({ accountId: id })
+
+      expect(result).toEqual([{ id, active, complement, neighborhood, number, street, surname, zipCode, accountId: id }])
     })
   })
 })
