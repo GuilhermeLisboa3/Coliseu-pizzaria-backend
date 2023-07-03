b5ab7cd71c173f1c3786df02a0a5d6f5f417950c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 14:39:11 -0300

    feat: ensure AddressRepository create address on success

--- src/infra/database/postgres/repositories/address.ts
+++ src/infra/database/postgres/repositories/address.ts
@@ -1,10 +1,16 @@
-import { ListAddressRepository } from '@/domain/contracts/database/repositories/address'
+import { ListAddressRepository, AddAddressRepository } from '@/domain/contracts/database/repositories/address'
 import { prisma, PrismaHelper } from '@/infra/database/postgres/helpers'
 
-export class AddressRepository implements ListAddressRepository {
+export class AddressRepository implements ListAddressRepository, AddAddressRepository {
   async list ({ accountId }: ListAddressRepository.Input): Promise<ListAddressRepository.Output> {
-    const address = await prisma.address.findMany({ where: { user_id: accountId } })
+    const addresses = await prisma.address.findMany({ where: { user_id: accountId } })
 
-    return address.map(address => PrismaHelper.addressMap(address))
+    return addresses.map(address => PrismaHelper.addressMap(address))
+  }
+
+  async create ({ accountId, ...input }: AddAddressRepository.Input): Promise<AddAddressRepository.Output> {
+    const address = await prisma.address.create({ data: { user_id: accountId, ...input } })
+
+    return PrismaHelper.addressMap(address)
   }
 }
