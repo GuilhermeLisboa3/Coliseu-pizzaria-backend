d72b040f5867775bee4f4b8bc425833ba77f2de2
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 16:40:49 -0300

    test: ensure ListAddressesController call listAddresses with correct values

--- tests/application/controllers/address/list-addresses.spec.ts
+++ tests/application/controllers/address/list-addresses.spec.ts
@@ -1,14 +1,24 @@
 import { ListAddressesController } from '@/application/controllers/address'
+import { accountParams } from '@/tests/mocks'
 import { Controller } from '@/application/controllers'
 
 describe('ListAddressesController', () => {
+  const { id: accountId } = accountParams
+  const listAddresses = jest.fn()
   let sut: ListAddressesController
 
   beforeEach(() => {
-    sut = new ListAddressesController()
+    sut = new ListAddressesController(listAddresses)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call listAddresses with correct values', async () => {
+    await sut.handle({ accountId })
+
+    expect(listAddresses).toHaveBeenCalledWith({ accountId })
+    expect(listAddresses).toHaveBeenCalledTimes(1)
   })
 })
