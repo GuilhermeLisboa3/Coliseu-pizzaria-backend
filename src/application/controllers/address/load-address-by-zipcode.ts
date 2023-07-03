d66a1690a4e9e1ee5a149a503396d8129902a2a9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 11:34:08 -0300

    feat: ensure LoadAddressByZipCodeController build Validators correctly

--- src/application/controllers/address/load-address-by-zipcode.ts
+++ src/application/controllers/address/load-address-by-zipcode.ts
@@ -1,10 +1,17 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { Validator, ValidationBuilder as Builder } from '@/application/validation'
 
 type HttpRequest = { zipCode: string }
 
 export class LoadAddressByZipCodeController extends Controller {
   async perform ({ zipCode }: HttpRequest): Promise<HttpResponse> {
     return { statusCode: 200, data: null }
+  }
+
+  buildValidators ({ zipCode }: HttpRequest): Validator[] {
+    return [
+      ...Builder.of(zipCode, 'zipCode').required().build()
+    ]
   }
 }
