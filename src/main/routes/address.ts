bb3aee35e5219c36bf6f173579b4e73191f2caa0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-03 17:03:37 -0300

    feat: ensure route /addresses return 200 on success

--- src/main/routes/address.ts
+++ src/main/routes/address.ts
@@ -1,10 +1,11 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
 import { auth } from '@/main/middlewares'
-import { makeLoadAddressByZipCodeController, makeAddAddressController } from '@/main/factories/application/controllers/address'
+import { makeLoadAddressByZipCodeController, makeAddAddressController, makeListAddressesController } from '@/main/factories/application/controllers/address'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.get('/address', auth, adapt(makeLoadAddressByZipCodeController()))
   router.post('/address', auth, adapt(makeAddAddressController()))
+  router.get('/addresses', auth, adapt(makeListAddressesController()))
 }
