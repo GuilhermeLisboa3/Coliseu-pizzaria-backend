09118d48a2baded80999c50371e8bef17bf346c8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 14:29:48 -0300

    feat: ensure UpdateAddressController call addAddress with correct values

--- src/application/controllers/address/update-address.ts
+++ src/application/controllers/address/update-address.ts
@@ -1,11 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
 import { Validator, ValidationBuilder as Builder } from '@/application/validation'
+import { UpdateAddress } from '@/domain/use-cases/address'
 
 type HttpRequest = { accountId: string, id: string, surname?: string, number?: number, complement?: string, active?: boolean }
 
 export class UpdateAddressController extends Controller {
-  async perform ({ id }: HttpRequest): Promise<HttpResponse> {
+  constructor (private readonly updateAddress: UpdateAddress) { super() }
+
+  async perform ({ id, accountId, active, complement, number, surname }: HttpRequest): Promise<HttpResponse> {
+    await this.updateAddress({ id, accountId, active, complement, number, surname })
     return { statusCode: 200, data: null }
   }
 
