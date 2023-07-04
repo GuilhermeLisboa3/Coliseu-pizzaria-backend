aef105a65e992c8614b7adab4283116810de56e1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 12:51:37 -0300

    test: ensure DeleteAddressController call deleteAddress with correct values

--- tests/application/controllers/address/delete-address.spec.ts
+++ tests/application/controllers/address/delete-address.spec.ts
@@ -1,14 +1,24 @@
 import { DeleteAddressController } from '@/application/controllers/address'
 import { Controller } from '@/application/controllers'
+import { addressParams } from '@/tests/mocks'
 
 describe('DeleteAddressController', () => {
   let sut: DeleteAddressController
+  const { id } = addressParams
+  const deleteAddress = jest.fn()
 
   beforeEach(() => {
-    sut = new DeleteAddressController()
+    sut = new DeleteAddressController(deleteAddress)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call deleteAddress with correct values', async () => {
+    await sut.handle({ id })
+
+    expect(deleteAddress).toHaveBeenCalledWith({ id })
+    expect(deleteAddress).toHaveBeenCalledTimes(1)
   })
 })
