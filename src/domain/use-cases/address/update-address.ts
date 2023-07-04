a0546454de320fe2b9516ff358f46a243bcb16aa
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 13:24:24 -0300

    feat: ensure updateAddressUseCase call ListAddressRepository if active exists

--- src/domain/use-cases/address/update-address.ts
+++ src/domain/use-cases/address/update-address.ts
@@ -1,12 +1,15 @@
-import { CheckAddressByIdRepository } from '@/domain/contracts/database/repositories/address'
+import { CheckAddressByIdRepository, ListAddressRepository } from '@/domain/contracts/database/repositories/address'
 import { FieldNotFoundError } from '@/domain/error'
 
-type Setup = (addressRepository: CheckAddressByIdRepository) => UpdateAddress
+type Setup = (addressRepository: CheckAddressByIdRepository & ListAddressRepository) => UpdateAddress
 type Input = { accountId: string, id: string, surname?: string, number?: number, complement?: string, active?: boolean }
 type Output = void
 export type UpdateAddress = (input: Input) => Promise<Output>
 
-export const updateAddressUseCase: Setup = (addressRepository) => async ({ id }) => {
+export const updateAddressUseCase: Setup = (addressRepository) => async ({ id, accountId, active }) => {
   const address = await addressRepository.checkById({ id })
   if (!address) throw new FieldNotFoundError('id')
+  if (active) {
+    await addressRepository.list({ accountId })
+  }
 }
