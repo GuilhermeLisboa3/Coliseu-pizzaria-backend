de12880772457ab03393025c2ffea7593b93de11
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 12:53:39 -0300

    feat: ensure DeleteAddressController return noContent on success

--- src/application/controllers/address/delete-address.ts
+++ src/application/controllers/address/delete-address.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '@/application/helpers'
+import { HttpResponse, noContent } from '@/application/helpers'
 import { DeleteAddress } from '@/domain/use-cases/address'
 
 type HttpRequest = { id: string }
@@ -9,6 +9,6 @@ export class DeleteAddressController extends Controller {
 
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
     await this.deleteAddress({ id })
-    return { statusCode: 200, data: null }
+    return noContent()
   }
 }
