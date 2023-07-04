3dcbc27741c7579dd11381df5e477c25dce1594c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 11:55:18 -0300

    feat: ensure DeleteAddressUseCas throw FieldNotFoundError if CheckAddressByIdRepository return false

--- src/domain/use-cases/address/delete-address.ts
+++ src/domain/use-cases/address/delete-address.ts
@@ -1,4 +1,5 @@
 import { CheckAddressByIdRepository } from '@/domain/contracts/database/repositories/address'
+import { FieldNotFoundError } from '@/domain/error'
 
 type Setup = (addressRepository: CheckAddressByIdRepository) => DeleteAddress
 type Input = { id: string }
@@ -6,5 +7,6 @@ type Output = void
 export type DeleteAddress = (input: Input) => Promise<Output>
 
 export const deleteAddressUseCase: Setup = (addressRepository) => async ({ id }) => {
-  await addressRepository.checkById({ id })
+  const address = await addressRepository.checkById({ id })
+  if (!address) throw new FieldNotFoundError('id')
 }
