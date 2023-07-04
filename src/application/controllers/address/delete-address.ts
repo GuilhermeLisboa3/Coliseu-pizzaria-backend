55d36e36afbe27afad192fbe28b0b9e1a38960f6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 12:50:39 -0300

    feat: ensure DeleteAddressController call deleteAddress with correct values

--- src/application/controllers/address/delete-address.ts
+++ src/application/controllers/address/delete-address.ts
@@ -1,10 +1,14 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { DeleteAddress } from '@/domain/use-cases/address'
 
 type HttpRequest = { id: string }
 
 export class DeleteAddressController extends Controller {
-  async perform (input: HttpRequest): Promise<HttpResponse> {
+  constructor (private readonly deleteAddress: DeleteAddress) { super() }
+
+  async perform ({ id }: HttpRequest): Promise<HttpResponse> {
+    await this.deleteAddress({ id })
     return { statusCode: 200, data: null }
   }
 }
