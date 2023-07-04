aa46fa9322f82a8580688341e8fd0a71db4f445e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 14:26:24 -0300

    feat: ensure UpdateAddressController build Validators correctly

--- src/application/controllers/address/update-address.ts
+++ src/application/controllers/address/update-address.ts
@@ -1,10 +1,17 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { Validator, ValidationBuilder as Builder } from '@/application/validation'
 
 type HttpRequest = { accountId: string, id: string, surname?: string, number?: number, complement?: string, active?: boolean }
 
 export class UpdateAddressController extends Controller {
   async perform ({ id }: HttpRequest): Promise<HttpResponse> {
     return { statusCode: 200, data: null }
+  }
+
+  buildValidators ({ id }: HttpRequest): Validator[] {
+    return [
+      ...Builder.of(id, 'id').required().build()
+    ]
   }
 }
