7c8c48d20d6e9ed6387a028a4300881bf5c13053
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 14:49:13 -0300

    feat: ensure route put /address return 400 if id not exists

--- src/main/routes/address.ts
+++ src/main/routes/address.ts
@@ -1,12 +1,19 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
 import { auth } from '@/main/middlewares'
-import { makeLoadAddressByZipCodeController, makeAddAddressController, makeListAddressesController, makeDeleteAddressController } from '@/main/factories/application/controllers/address'
+import {
+  makeLoadAddressByZipCodeController,
+  makeAddAddressController,
+  makeListAddressesController,
+  makeDeleteAddressController,
+  makeUpdateAddressController
+} from '@/main/factories/application/controllers/address'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.get('/address', auth, adapt(makeLoadAddressByZipCodeController()))
   router.post('/address', auth, adapt(makeAddAddressController()))
+  router.put('/address', auth, adapt(makeUpdateAddressController()))
   router.get('/addresses', auth, adapt(makeListAddressesController()))
   router.delete('/address/:id', auth, adapt(makeDeleteAddressController()))
 }
