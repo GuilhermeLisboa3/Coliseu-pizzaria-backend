b062c50a11dc53cc1ba3c6b52f4bbbe98fb4d975
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 14:27:50 -0300

    test: ensure UpdateAddressController build Validators correctly

--- tests/application/controllers/address/update-address.spec.ts
+++ tests/application/controllers/address/update-address.spec.ts
@@ -1,8 +1,11 @@
 import { UpdateAddressController } from '@/application/controllers/address'
 import { Controller } from '@/application/controllers'
+import { addressParams } from '@/tests/mocks'
+import { RequiredValidation } from '@/application/validation'
 
 describe('UpdateAddressController', () => {
   let sut: UpdateAddressController
+  const { id, complement } = addressParams
 
   beforeEach(() => {
     sut = new UpdateAddressController()
@@ -10,5 +13,13 @@ describe('UpdateAddressController', () => {
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ id, complement, accountId: id })
+
+    expect(validators).toEqual([
+      new RequiredValidation(id, 'id')
+    ])
   })
 })
