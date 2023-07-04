c55e1e94606d515609b0f63c95567bb42c957900
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-04 13:16:41 -0300

    feat: ensure updateAddressUseCas throw FieldNotFoundError if CheckAddressByIdRepository return false

--- src/domain/use-cases/address/update-address.ts
+++ src/domain/use-cases/address/update-address.ts
@@ -1,4 +1,5 @@
 import { CheckAddressByIdRepository } from '@/domain/contracts/database/repositories/address'
+import { FieldNotFoundError } from '@/domain/error'
 
 type Setup = (addressRepository: CheckAddressByIdRepository) => UpdateAddress
 type Input = { accountId: string, id: string, surname?: string, number?: number, complement?: string, active?: boolean }
@@ -6,5 +7,6 @@ type Output = void
 export type UpdateAddress = (input: Input) => Promise<Output>
 
 export const updateAddressUseCase: Setup = (addressRepository) => async ({ id }) => {
-  await addressRepository.checkById({ id })
+  const address = await addressRepository.checkById({ id })
+  if (!address) throw new FieldNotFoundError('id')
 }
