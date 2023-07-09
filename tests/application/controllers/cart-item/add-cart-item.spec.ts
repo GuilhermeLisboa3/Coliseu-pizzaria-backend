2210f819ebac0900957c11e2c2d8df9e9117c61c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-09 15:06:24 -0300

    test: ensure AddCartItemController call addCartItem with correct values

--- tests/application/controllers/cart-item/add-cart-item.spec.ts
+++ tests/application/controllers/cart-item/add-cart-item.spec.ts
@@ -1,14 +1,25 @@
+import { accountParams, productParams } from '@/tests/mocks'
 import { AddCartItemController } from '@/application/controllers/cart-item'
 import { Controller } from '@/application/controllers'
 
 describe('AddCartItemController', () => {
   let sut: AddCartItemController
+  const addCartItem = jest.fn()
+  const { id: accountId } = accountParams
+  const { id: productId } = productParams
 
   beforeEach(() => {
-    sut = new AddCartItemController()
+    sut = new AddCartItemController(addCartItem)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call addCartItem with correct values', async () => {
+    await sut.handle({ accountId, productId })
+
+    expect(addCartItem).toHaveBeenCalledWith({ accountId, productId })
+    expect(addCartItem).toHaveBeenCalledTimes(1)
   })
 })
