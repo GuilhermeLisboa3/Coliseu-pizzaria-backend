23ad31dd8f2087300e9b7684479e8e42e4098042
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-09 15:05:23 -0300

    feat: ensure AddCartItemController call addCartItem with correct values

--- src/application/controllers/cart-item/add-cart-item.ts
+++ src/application/controllers/cart-item/add-cart-item.ts
@@ -1,10 +1,14 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { AddCartItem } from '@/domain/use-cases/cart-item'
 
 type HttpRequest = { accountId: string, productId: string }
 
 export class AddCartItemController extends Controller {
+  constructor (private readonly addCartItem: AddCartItem) { super() }
+
   async perform ({ accountId, productId }: HttpRequest): Promise<HttpResponse> {
+    await this.addCartItem({ accountId, productId })
     return { statusCode: 200, data: null }
   }
 }
