c1f39cebfd68c6d814657b31b4d197093979b30d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-09 14:43:35 -0300

    feat: ensure CartRepository load return cart on success

--- src/infra/database/postgres/repositories/cart.ts
+++ src/infra/database/postgres/repositories/cart.ts
@@ -1,10 +1,16 @@
-import { AddCartRepository } from '@/domain/contracts/database/repositories/cart'
+import { AddCartRepository, LoadCartRepository } from '@/domain/contracts/database/repositories/cart'
 import { prisma } from '@/infra/database/postgres/helpers'
 
-export class CartRepository implements AddCartRepository {
+export class CartRepository implements AddCartRepository, LoadCartRepository {
   async create ({ accountId }: AddCartRepository.Input): Promise<AddCartRepository.Output> {
     const { id, userId } = await prisma.cart.create({ data: { userId: accountId } })
 
     return { id, accountId: userId }
+  }
+
+  async load ({ accountId }: LoadCartRepository.Input): Promise<LoadCartRepository.Output> {
+    const cart = await prisma.cart.findFirst({ where: { userId: accountId } })
+    if (!cart) return null
+    return { id: cart.id, accountId: cart.userId }
   }
 }
