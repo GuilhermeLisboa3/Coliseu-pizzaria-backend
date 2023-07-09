82c5ad5938690045ed9e71d4cb38248766731fd8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-09 15:07:16 -0300

    test: ensure AddCartItemController return badRequest if addCartItem return FieldNotFoundError

--- tests/application/controllers/cart-item/add-cart-item.spec.ts
+++ tests/application/controllers/cart-item/add-cart-item.spec.ts
@@ -1,6 +1,7 @@
 import { accountParams, productParams } from '@/tests/mocks'
 import { AddCartItemController } from '@/application/controllers/cart-item'
 import { Controller } from '@/application/controllers'
+import { FieldNotFoundError } from '@/domain/error'
 
 describe('AddCartItemController', () => {
   let sut: AddCartItemController
@@ -21,5 +22,13 @@ describe('AddCartItemController', () => {
 
     expect(addCartItem).toHaveBeenCalledWith({ accountId, productId })
     expect(addCartItem).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return badRequest if addCartItem return FieldNotFoundError', async () => {
+    addCartItem.mockRejectedValueOnce(new FieldNotFoundError('productId'))
+    const { statusCode, data } = await sut.handle({ accountId, productId })
+
+    expect(statusCode).toBe(400)
+    expect(data).toEqual(new FieldNotFoundError('productId'))
   })
 })
