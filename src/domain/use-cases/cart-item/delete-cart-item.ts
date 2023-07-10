2a37b7a88d2d2b210d2a366e03a11411a2b2f6a0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-10 10:57:05 -0300

    feat: ensure DeleteCartItemUseCase call LoadCartRepository with correct value

--- src/domain/use-cases/cart-item/delete-cart-item.ts
+++ src/domain/use-cases/cart-item/delete-cart-item.ts
@@ -1,12 +1,14 @@
+import { LoadCartRepository } from '@/domain/contracts/database/repositories/cart'
 import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
 import { FieldNotFoundError } from '@/domain/error'
 
-type Setup = (productRepository: LoadProductRepository) => DeleteCartItem
+type Setup = (productRepository: LoadProductRepository, cartRepository: LoadCartRepository) => DeleteCartItem
 type Input = { accountId: string, productId: string }
 type Output = void
 export type DeleteCartItem = (input: Input) => Promise<Output>
 
-export const deleteCartItemUseCase: Setup = (productRepository) => async ({ accountId, productId }) => {
+export const deleteCartItemUseCase: Setup = (productRepository, cartRepository) => async ({ accountId, productId }) => {
   const product = await productRepository.load({ id: productId })
   if (!product) throw new FieldNotFoundError('productId')
+  await cartRepository.load({ accountId })
 }
