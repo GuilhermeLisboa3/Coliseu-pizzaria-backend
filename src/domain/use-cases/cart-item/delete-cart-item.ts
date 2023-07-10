3468cac003f3b36ecafda7d133d0f351093e16d7
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-10 11:05:40 -0300

    feat: ensure DeleteCartItemUseCase call LoadCartItemRepository with correct value

--- src/domain/use-cases/cart-item/delete-cart-item.ts
+++ src/domain/use-cases/cart-item/delete-cart-item.ts
@@ -1,15 +1,21 @@
 import { LoadCartRepository } from '@/domain/contracts/database/repositories/cart'
+import { LoadCartItemRepository } from '@/domain/contracts/database/repositories/cart-item'
 import { LoadProductRepository } from '@/domain/contracts/database/repositories/product'
 import { FieldNotFoundError } from '@/domain/error'
 
-type Setup = (productRepository: LoadProductRepository, cartRepository: LoadCartRepository) => DeleteCartItem
+type Setup = (
+  productRepository: LoadProductRepository,
+  cartRepository: LoadCartRepository,
+  cartItemRepository: LoadCartItemRepository
+) => DeleteCartItem
 type Input = { accountId: string, productId: string }
 type Output = void
 export type DeleteCartItem = (input: Input) => Promise<Output>
 
-export const deleteCartItemUseCase: Setup = (productRepository, cartRepository) => async ({ accountId, productId }) => {
+export const deleteCartItemUseCase: Setup = (productRepository, cartRepository, cartItemRepository) => async ({ accountId, productId }) => {
   const product = await productRepository.load({ id: productId })
   if (!product) throw new FieldNotFoundError('productId')
   const cart = await cartRepository.load({ accountId })
   if (!cart) throw new FieldNotFoundError('cart')
+  await cartItemRepository.load({ cartId: cart.id, productId })
 }
