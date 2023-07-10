0211debbd1e9ebe9d01cb893a9d6569d100948e4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-10 14:10:26 -0300

    feat: ensure LoadCartWithProductsController return ok on success

--- src/application/controllers/cart/load-cart-with-products.ts
+++ src/application/controllers/cart/load-cart-with-products.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '@/application/helpers'
+import { HttpResponse, ok } from '@/application/helpers'
 import { LoadCartWithProducts } from '@/domain/use-cases/cart'
 
 type HttpRequest = { accountId: string }
@@ -8,7 +8,7 @@ export class LoadCartWithProductsController extends Controller {
   constructor (private readonly loadCartWithProducts: LoadCartWithProducts) { super() }
 
   async perform ({ accountId }: HttpRequest): Promise<HttpResponse> {
-    await this.loadCartWithProducts({ accountId })
-    return { data: null, statusCode: 200 }
+    const cartWithProducts = await this.loadCartWithProducts({ accountId })
+    return ok(cartWithProducts)
   }
 }
