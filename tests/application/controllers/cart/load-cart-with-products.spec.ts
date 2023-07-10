f2cf7755c3205e71d826ab5f21eea6c7dcfa9e52
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-10 14:09:12 -0300

    test: ensure LoadCartWithProductsController call loadCartWithProducts with correct values

--- tests/application/controllers/cart/load-cart-with-products.spec.ts
+++ tests/application/controllers/cart/load-cart-with-products.spec.ts
@@ -1,14 +1,24 @@
 import { LoadCartWithProductsController } from '@/application/controllers/cart'
+import { accountParams } from '@/tests/mocks'
 import { Controller } from '@/application/controllers'
 
 describe('LoadCartWithProductsController', () => {
   let sut: LoadCartWithProductsController
+  const loadCartWithProducts = jest.fn()
+  const { id: accountId } = accountParams
 
   beforeEach(() => {
-    sut = new LoadCartWithProductsController()
+    sut = new LoadCartWithProductsController(loadCartWithProducts)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call loadCartWithProducts with correct values', async () => {
+    await sut.handle({ accountId })
+
+    expect(loadCartWithProducts).toHaveBeenCalledWith({ accountId })
+    expect(loadCartWithProducts).toHaveBeenCalledTimes(1)
   })
 })
