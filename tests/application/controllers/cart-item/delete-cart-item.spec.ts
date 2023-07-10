9252d14d9ecb90adca1b18465835c6c012886a9f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-10 12:07:09 -0300

    test: ensure DeleteCartItemController call deleteCartItem with correct value

--- tests/application/controllers/cart-item/delete-cart-item.spec.ts
+++ tests/application/controllers/cart-item/delete-cart-item.spec.ts
@@ -1,14 +1,25 @@
 import { DeleteCartItemController } from '@/application/controllers/cart-item'
 import { Controller } from '@/application/controllers'
+import { accountParams, productParams } from '@/tests/mocks'
 
 describe('DeleteCartItemController', () => {
   let sut: DeleteCartItemController
+  const deleteCartItem = jest.fn()
+  const { id } = productParams
+  const { id: accountId } = accountParams
 
   beforeEach(() => {
-    sut = new DeleteCartItemController()
+    sut = new DeleteCartItemController(deleteCartItem)
   })
 
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call deleteCartItem with correct values', async () => {
+    await sut.handle({ accountId, id })
+
+    expect(deleteCartItem).toHaveBeenCalledWith({ accountId, productId: id })
+    expect(deleteCartItem).toHaveBeenCalledTimes(1)
   })
 })
