7e0be8dda3a637d0d9eddb5c7dfad1ffe9c5a7ad
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-10 13:57:18 -0300

    test: ensure CartRepository loadWithProducts return cart with products on success

--- tests/infra/database/postgres/repositories/cart.spec.ts
+++ tests/infra/database/postgres/repositories/cart.spec.ts
@@ -1,10 +1,11 @@
 import { CartRepository } from '@/infra/database/postgres/repositories'
-import { accountParams } from '@/tests/mocks'
+import { accountParams, productParams } from '@/tests/mocks'
 import { prisma } from '@/infra/database/postgres/helpers'
 
 describe('CartRepository', () => {
   let sut: CartRepository
   const { id, email, name, password } = accountParams
+  const { available, description, picture, price } = productParams
 
   beforeEach(async () => {
     await prisma.$queryRaw`DELETE FROM "cartItems"`
@@ -13,6 +14,8 @@ describe('CartRepository', () => {
     await prisma.$queryRaw`DELETE FROM products`
     await prisma.$queryRaw`DELETE FROM categories`
     await prisma.user.create({ data: { id, name, password, email } })
+    await prisma.category.create({ data: { id, name } })
+    await prisma.product.create({ data: { id, available, description, name, picture, price, categoryId: id } })
     sut = new CartRepository()
   })
 
@@ -37,6 +40,21 @@ describe('CartRepository', () => {
       const result = await sut.load({ accountId: id })
 
       expect(result).toBeNull()
+    })
+  })
+
+  describe('loadWithProducts()', () => {
+    it('should return cart with products on success', async () => {
+      await prisma.cart.create({ data: { id, userId: id } })
+      await prisma.cartItem.create({ data: { id, cartId: id, productId: id, quantity: 3 } })
+
+      const result = await sut.loadWithProducts({ accountId: id })
+
+      expect(result).toEqual({
+        id,
+        accountId: id,
+        products: [{ id, name, description, available, picture, price, categoryId: id, quantity: 3 }]
+      })
     })
   })
 })
