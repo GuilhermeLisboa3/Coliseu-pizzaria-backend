43338bdea558b275ad9124d359762086cc31d9cc
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-10 12:23:16 -0300

    feat: ensure route delete /cart-item/:id return 400 if product not exists

--- src/main/routes/cartItem.ts
+++ src/main/routes/cartItem.ts
@@ -1,9 +1,10 @@
 import { expressRouterAdapter as adapt } from '@/main/adapters'
 import { auth } from '@/main/middlewares'
-import { makeAddCartItemController } from '@/main/factories/application/controllers/cart-item'
+import { makeAddCartItemController, makeDeleteCartItemController } from '@/main/factories/application/controllers/cart-item'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/cart-item/:id', auth, adapt(makeAddCartItemController()))
+  router.delete('/cart-item/:id', auth, adapt(makeDeleteCartItemController()))
 }
