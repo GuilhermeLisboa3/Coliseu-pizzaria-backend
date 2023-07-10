d215ffdf44e0d77f83e2547af8fff58ea7590c9e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-10 12:06:11 -0300

    feat: ensure DeleteCartItemController call deleteCartItem with correct value

--- src/application/controllers/cart-item/delete-cart-item.ts
+++ src/application/controllers/cart-item/delete-cart-item.ts
@@ -1,10 +1,14 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
+import { DeleteCartItem } from '@/domain/use-cases/cart-item'
 
 type HttpRequest = { accountId: string, id: string }
 
 export class DeleteCartItemController extends Controller {
+  constructor (private readonly deleteCartItem: DeleteCartItem) { super() }
+
   async perform ({ accountId, id }: HttpRequest): Promise<HttpResponse> {
+    await this.deleteCartItem({ accountId, productId: id })
     return { statusCode: 200, data: null }
   }
 }
